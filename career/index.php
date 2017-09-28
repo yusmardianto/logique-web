e5a5809754026621c8c7a82ae930810cac4b1012
Gilbertus Nico <nico@logique.co.id>
2017-09-28 17:50:08 +0700

    Add career page

---
+++ career/index.php
@@ -0,0 +1,353 @@
+﻿<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="PT. Logique Digital Indonesia dapat membantu kalian dalam Internet Marketing, responsive Web development, SEO, dll.">
+	<meta name="keywords" content="Career Logique, Logique Job , Lowongan , karir">
+	<meta name="author" content="PT. Logique Digital Indonesia">
+	<meta property="og:title" content="Karir | PT. Logique Digital Indonesia">
+	<meta property="og:type" content="website">
+	<meta property="og:site_name" content="Logique">
+	<meta property="og:url" content="https://www.logique.co.id/career">
+	<meta property="og:image" content="https://www.logique.co.id/img/logo.png">
+	<meta property="og:description" content="PT. Logique Digital Indonesia dapat membantu kalian dalam Internet Marketing, responsive Web development, SEO, dll.">
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-105878648-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+    <title>Karir | PT. Logique Digital Indonesia</title>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+	
+    <link href="../css/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+	<script src="https://use.fontawesome.com/df558bb4a1.js"></script>
+    
+    </head>
+	<body class="career-page">
+		<section class="maintop">
+			<div class="bg-white-opacity opacity-career"></div>
+			<video poster="../img/bg-career-new-main.png" id="bgvid2" playsinline="" autoplay="" muted="" loop="">
+				<source src="../img/3734627.mp4" type="video/mp4">
+			</video>
+			<div class="header-career">
+				<div><img src="../img/logo.png" alt="Logo"/><br/>CAREER</div>
+			</div>
+			<div class="box-home-career col-md-offset-2 col-md-8">
+				<div class="valign-center">
+					<p>
+					LOGIQUE is in a process to be the No.1 Web developing company in Indonesia. We already have numbers of big clients. You can be on a middle-management positions depending on your passion, as we are still in the middle of the flourishment and flexible. We prepare the environment and promise career growth. We support you to grow your both technic and work skills. We get rid of "Nonsense" from our work as much as possible. We prepare the environment that you can enjoy working effectively.
+					</p>
+					<br/>
+					<div class="div-black-career">
+						Let's work hard, enjoy hard, and grow up hard together
+					</div>
+				</div>
+			</div>
+			<div id="scroll">
+				<a class="smooth" href="#navbar"><span><img src="../img/scrollarrow.png"/></span></a>
+			</div>
+		</section>
+		
+		<nav class="navbar" id="navbar">
+		  <div class="container">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>  
+			  </button>
+			  <a class="navbar-brand" href="#"><img src="../img/logo.png" alt="Logo"/></a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+			  <ul class="nav navbar-nav navbar-right">
+				<li><a class="smooth" href="#benefits">Benefits</a></li>
+				<li><a class="smooth" href="#internal-rules">Internal Rules</a></li>
+				<li><a class="smooth" href="#our-policy">Vision & Policy</a></li>
+				<li><a class="smooth" href="#to-candidates">To Candidates</a></li>
+				<li><a class="smooth" href="#job-opening">Job Opening</a></li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+		
+		<img class="image-career" src="../img/career-main.png" alt="Logo"/>
+		
+		<section id="benefits">
+			<div class="header-career">
+				<div>BENEFITS</div>
+			</div>
+			<div class="row no-gap" style="margin:0">
+				<div class="col-md-6">
+					<div class="col-benefit benefit-col-1">
+						<div class="valign-center">
+							<span class="title-benefit-col-1">Working in LOGIQUE :</span>
+							<p class="content-benefit-col-1">
+								LOGIQUE is a web development company that is founded and operated by a Japanese founder in Indonesia. For that reason, LOGIQUE has many opportunities to work with international clients and you will be able to learn the way of work with international standards. Most of our projects / clients require high standards of design, programming, and the management skill, but this challenge <span>will make you grow up</span>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="col-benefit benefit-col-2">
+						<div class="row no-gap" style="margin:0">
+							<div class="col-sm-4 hidden-xs">
+								<div class="content-benefits blank">
+								</div>
+							</div>
+							<div class="col-sm-4 col-xs-6">
+								<div class="content-benefits">
+									<div class="valign-center great-view">
+										<img src="../img/benefits1.png" alt="Great View"/>
+										a new office <br/>that has <br/><span>GREAT <br/>VIEW</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4 col-xs-6">
+								<div class="content-benefits">
+									<div class="valign-center get-bonus">
+										<span class="first-element"> get <b>bonus</b> </span><br/>
+										<span class="second-element"> every month </span><br/>
+										depending on your performance
+										<img src="../img/benefits2.png" alt="Get Bonus"/>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4 col-xs-6">
+								<div class="content-benefits black-bg">
+									<div class="valign-center">
+										Other<br/>Benefits:
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4 col-xs-6">
+								<div class="content-benefits growing-company">
+									<div class="valign-center text-right">
+										<img src="../img/benefits3.png" alt="Growing Company"/>
+										work in <br/>
+										<span>Growing<br/>Company</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4 col-xs-6">
+								<div class="content-benefits flexible">
+									<div class="valign-center text-center">
+										<img src="../img/benefits4.png" alt="Flexible" class="img-responsive center-block"/>
+										<span>Flexible</span> rules <br/>
+										& working hours
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4 col-xs-6">
+								<div class="content-benefits language-indonesia">
+									<div class="valign-center text-center">
+										<img src="../img/benefits5.png" alt="Language" class="img-responsive center-block"/>
+										<span>Language: Indonesia</span><br/>
+										though we are not<br/> Indonesian company
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4 col-xs-6">
+								<div class="content-benefits many-fields">
+									<div class="valign-center">
+										<img src="../img/benefits6.png" alt="Many Fields"/>
+										<p>many fields of work from <span>consulting</span> up to <span>system development.</span></p>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4 col-xs-6">
+								<div class="content-benefits middle-management">
+									<div class="valign-center">
+										<img src="../img/benefits7.png" alt="Middle Management" class="img-responsive center-block"/>
+										Can be on <br/>
+										<span>middle - management positions</span> <br/>
+										depending on your passion.
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section id="internal-rules">
+			<div class="container">
+				<div class="title text-center">Introduction of Our Internal Rules</div>
+				<div class="row no-gap" style="padding: 0 15px;">
+					<div class="col-md-4">
+						<img alt="Working Hours" src="../img/internal1.png"/>
+						<div class="subtitle text-center">Working Hours</div>
+						<p>
+							You can come to the office anytime during 8-10 am as long as you work for 8 hours. And of course Saturday and Sunday are your holidays.
+						</p>
+					</div>
+					<div class="col-md-4">
+						<img alt="Daily Reports" src="../img/internal2.png"/>
+						<div class="subtitle text-center">Daily Report</div>
+						<p>
+							You can read everyone's daily report so that you can understand each other better seeing what your friends are doing, what they are thinking, and their challenges. Evaluation will be conducted by daily report as well.
+						</p>
+					</div>
+					<div class="col-md-4">
+						<img alt="Monthly Evaluation" src="../img/internal3.png"/>
+						<div class="subtitle text-center">Monthly evaluation & Bonus</div>
+						<p>
+							You will be evaluated every month and you will get paid performance bonus if your performance was outstanding. We will choose the best staff every month (can be more than 1 person). The best staff will also get monthly bonus as well. Plus, you will get a special bonus if you accomplish something notable.
+						</p>
+					</div>
+				</div>
+			</div>			
+		</section>
+		
+		<section id="our-policy">
+			<div class="container">
+				<div class="title">Our Vision and Policy</div>
+				<div class="row">
+					<div class="col-md-offset-1 col-md-10">
+						<p>
+						LOGIQUE believes it is important to always think Logically. We will get rid of everything that are not necessary or nonsense. We think and take actions logically considering clients' benefits and our benefits. The amount / energy of action after thinking will be also considered importantly. Both persons who cannot think and who cannot take action will not be evaluated in a good way at LOGIQUE.<br/><br/>
+						We are aiming to be the strong company / the strong individuals that can survive in any times or period seeing current and future situation of Web industry. It shouldn't be so hard for you to imagine that the trends change in 1-2 years and your current skill will be useless after 5 years from now. In this kind of situation, what is important is to obtain skills or strength (core competence) other than leaning necessary skills currently so that you can adopt yourself no matter how the trends change in the future. You will not be a truly strong professional if you keep on doing whatever that you already can do. LOGIQUE let you try many tasks in which you have no experience if you have passion and potential. We train and assign works to everyone to deepen each core skills.
+						</p>
+					</div>
+				</div>
+			</div>
+			<img src="../img/policy.png" alt="Vision and Policy" class="vision-img"/>
+			<div class="container" id="to-candidates">
+				<div class="row">
+					<div class="col-md-offset-1 col-md-10">
+						<div class="subtitle">Message for Candidates</div>
+						It is very important for your life about where & how you work. If you are living hard, thinking that work is important in your life, and thinking to contribute to a company, we promise to prepare the right environment for you. Let’s grow up together. 
+						<br/><br/>
+						<div class="subtitle">Job Opening</div>
+						We are always looking for candidates for the positions below. Feel free to contact us.
+						<br/><br/><br/>
+						<img class="img-responsive center-block" src="../img/job-application-en.png"/>
+						<br/>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section id="job-opening">
+			<div class="one-job-opening">
+				<div class="col-sm-4" style="padding:0">
+					<div class="div-job title-job">
+						<div class="valign-center">
+							<img alt="Project Manager" src="../img/proj-manager.png" class="img-responsive"/>
+							Project Manager
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-8">
+					<div class="div-job desc-job">
+						<b>Job Description:</b><br/>
+						Manage tasks and schedule communicating with both development team and client.<br/>
+						Create documents for project requirement.<br/>
+						Total project management (e.g. propose or planning).<br/><br/>
+
+						<b>Requirement:</b><br/>
+						Experience in IT / WEB more than 3 years.<br/>
+						Experience / Knowledge of programming and design.<br/>
+						English (Business Level).<br/><br/>
+
+						<b>Ideal Candidate:</b><br/>
+						A person who likes Web / Internet and use it all the time.<br/>
+						A person who can think logically and take actions.<br/>
+						A person who has knowledge in from Marketing Technology and has various curiosity.<br/>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="separator-career"></div>
+			<div class="one-job-opening">
+				<div class="col-sm-4" style="padding:0">
+					<div class="div-job title-job">
+						<div class="valign-center">
+							<img alt="Designer" src="../img/designer.png" class="img-responsive"/>
+							Designer
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-8">
+					<div class="div-job desc-job">
+						<b>Job Description:</b><br/>
+						UI design for Web and Mobile apps.<br/>
+						Design for Video and printings products.<br/>
+						Planning, art direction, and other support for those design works above.<br/><br/>
+
+						<b>Requirement:</b><br/>
+						Skills to use design tools (Photoshop, Illustrator, etc,.)<br/>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</section>
+		
+		<div class="bg-footer-new-career">
+			<span>Have skill, come &amp; join us?</span><br>
+			<div class="text-center">
+				<a href="#" class="btn btn-contact-us-black">
+					Join Us
+				</a>
+			</div>
+		</div>
+		
+		<div class="copyright-career">
+			© 2017 Logique Digital Indonesia
+		</div>
+		
+		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="../js/jquery.matchHeight.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/slick.min.js"></script>
+		<script>
+		$(function() {
+			$('.div-job').matchHeight();
+			$('.col-benefit').matchHeight();
+		});	
+		</script>
+		<!--<a href="#navbar" class="smooth scrollToTop"><i class="fa fa-hand-o-up"></i><br/>Back to Menu</a>-->
+		<script>
+			$(function() {
+				$('.smooth').click(function() {
+				  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+					var target = $(this.hash);
+					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+					if (target.length) {
+					  $('html, body').animate({
+						scrollTop: target.offset().top
+					  }, 1000);
+					  return false;
+					}
+				  }
+				});
+			});
+			
+			$(document).ready(function(){	
+				//Check to see if the window is top if not then display button
+				$(window).scroll(function(){
+					if ($(this).scrollTop() > 100) {
+						$('.scrollToTop').fadeIn();
+					} else {
+						$('.scrollToTop').fadeOut();
+					}
+				});	
+				/* //Click event to scroll to top
+				$('.scrollToTop').click(function(){
+					$('html, body').animate({scrollTop : 0},800);
+					return false;
+				}); */
+			});
+		</script>
+	</body>
+</html>
