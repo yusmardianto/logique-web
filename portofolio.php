e18f20975b67c997014dcb7dd78b6c409ed2d331
Gilbertus Nico <nico@logique.co.id>
2018-02-27 14:06:22 +0700

    update portfolio

--- portofolio.php
+++ portofolio.php
@@ -217,7 +217,7 @@
 							</div>
 							<div class="clearfix"></div>
 							<div class="col-sm-4">
-								<a href="http://jjs.logique.co.id/" target="_blank">
+								<a href="http://jjs.or.id/" target="_blank">
 								<img src="/img/portfolio/jjs.png" class="img-responsive center-block">
 								Jakarta Japanese School<br><br>
 								</a>
@@ -247,7 +247,7 @@
 								</a>
 							</div>
 							 <div class="col-sm-4">
-								<a href="http://kindergarten.jjs.logique.co.id/" target="_blank">
+								<a href="http://kindergarten.jjs.or.id/" target="_blank">
 									<img src="/img/portfolio/kindergarten-jjs.png" class="img-responsive center-block">
 									Kindergarten - Jakarta Japanese School<br><br>
 								</a>
@@ -257,6 +257,12 @@
 								<a href="http://hoyu.co.id/" target="_blank">
 									<img src="/img/portfolio/hoyu.png" class="img-responsive center-block">
 									Hoyu<br><br>
+								</a>
+							</div>	
+							<div class="col-sm-4">
+								<a href="https://4dentist.jp/" target="_blank">
+									<img src="/img/portfolio/4dentist.png" class="img-responsive center-block">
+									4dentist<br><br>
 								</a>
 							</div>							 
 							
