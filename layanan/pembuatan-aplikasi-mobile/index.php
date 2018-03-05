9b19f42c96c8e4f8340daf908ced2cc688a9dc0b
Gilbertus Nico <nico@logique.co.id>
2018-03-05 15:09:22 +0700

    LP about Mobile App

---
+++ layanan/pembuatan-aplikasi-mobile/index.php
@@ -0,0 +1,219 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="PT LOGIQUE DIGITAL INDONESIA ">
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <title>Aplikasi Mobile | Logique Digital Indonesia</title>
+	<link href="/css/bootstrap.min.css" rel="stylesheet">	 
+    <link href="css/style.css" rel="stylesheet">
+    <link href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css" rel="stylesheet" type="text/css" />
+	<script src="https://use.fontawesome.com/df558bb4a1.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Rokkitt:300,400,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-105878648-1', 'auto');
+	ga('send', 'pageview');
+
+	</script>
+</head>
+	<body class="mobileapp-page">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<img class="img-responsive" src="img/logo_logique.png"/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-7">
+						<br/><br/><br/>
+						<h1>Miliki <span class="yellow">“Aplikasi mobile”</span> yang memenuhi segala harapan anda</h1>
+						<p>	
+						Sebuah mobile aplikasi dapat meningkatkan kinerja marketing serta dapat meningkatkan
+						brand awareness perusahaan anda.
+						</p>
+						<div class="visible-md visible-lg"><br/><br/><br/><br/><br/><br/><br/><br/><br/></div>
+					</div>
+					<div class="col-md-5">
+						<img src="img/hp-header.png" class="hp-header img-responsive center-block"/>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="hubungi-kami-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="/hubungi-kami.php" target="_blank"><span class="purple">Hubungi kami</span></a> untuk informasi lebih lanjut.
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<br/><br/>
+			<div class="row">
+				<div class="col-md-6">
+					<img src="img/mobile-phone.png" class="img-responsive center-block mobile-phone"/>
+				</div>
+				<div class="col-md-6">
+					<h2 class="first">Alasan Perlunya Sebuah <span>Aplikasi Mobile</span></h2>
+					<div class="hidden-sm hidden-xs"><br/></div>
+					<br/>
+					<p>
+						Di era digital saat ini penggunaan smartphone atau telepon pintar menjadi begitu populer di masyarakat, ini mendorong beberapa perusahan membuat aplikasi mobile. Aplikasi mobile ini dapat berupa produk mereka atau sebagai alat penunjang kinerja mereka. <br/><br/>
+						Oleh sebab itu memiliki sebuah aplilkasi mobile dapat meningkatkan nilai perusahaan anda baik secara financial dan citra perusahaan anda dimata konsumen atau insvestor anda.
+					</p>
+				</div>
+			</div>
+			<br/><br/>
+		</div>
+		<div class="solusi">
+			<div class="container">
+				<br/><br/>
+				<div class="row row-center-md">
+					<div class="col-md-offset-1 col-md-3">
+						<div class="title">Solusi yang di peroleh dari pengambangan <span><b>Aplikasi Mobile</b></span></div>
+					</div>
+					<div class="col-md-8">
+						<ul class="fa-ul">
+							<li><span class="fa-li"><i class="fa fa-check-square"></i></span>Aplikasi mobile sebagai produk dan aset yang menghasilkan.</li>
+							<li><span class="fa-li"><i class="fa fa-check-square"></i></span>Aplikasi mobile sebagai media penghubung antara perusahan dan karyawan.</li>
+							<li><span class="fa-li"><i class="fa fa-check-square"></i></span>Aplikasi mobile sebagai alat pendukung pemasaran produk dan jasa anda.</li>
+							<li><span class="fa-li"><i class="fa fa-check-square"></i></span>Aplikasi mobile sebagai aplikasi pendukung pengunaan produk anda.</li>
+						</ul>
+					</div>
+				</div>
+				<br/><br/>
+			</div>
+		</div>
+		<div class="slider-mobile">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<img src="img/device.png" class="device hidden-xs"/>
+						<img src="img/device-m.png" class="device visible-xs"/>
+						<div class="slider-app">
+						  <div><img src="img/sunlife.png" class="img-responsive center-block"/></div>
+						  <div><img src="img/frangipani.png" class="img-responsive center-block"/></div>
+						  <div><img src="img/otomart.png" class="img-responsive center-block"/></div>
+						  <div><img src="img/otomart.png" class="img-responsive center-block"/></div>
+						</div>		
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="div-form">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-offset-2 col-md-8">
+						Hubungi kami untuk mendapatkan <b>penawaran</b> terbaik
+						<br/>
+						<form>
+							<div class="form-group">
+								<div class="row">
+									<div class="col-md-6">
+										<input type="text" name="name" placeholder="Nama Anda" class="form-control name-input"/>
+									</div>
+									<div class="col-md-6">
+										<input type="email" name="name" placeholder="Alamat Email" class="form-control"/>
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="row">
+									<div class="col-md-12">
+										<textarea class="form-control" placeholder="Pertanyaan" name="pertanyaan"></textarea>
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="sr-only">Recaptcha</div>
+									</div>
+									<div class="col-md-6">
+										<button type="submit" class="pull-right btn btn-submit">Kirim</button>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="buat-sekarang">
+			<div class="container">
+				<div class="row row-center-md">
+					<div class="col-md-6 font20">
+						Memiliki pertanyaan dan perlu informasi lebih lanjut ?
+						<div class="hidden-lg hidden-md"><br/></div>
+					</div>
+					<div class="col-md-6">
+						<a href="/hubungi-kami.php" target="_blank" class="btn btn-buat-sekarang">BUAT SEKARANG</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<div class="title">Tentang Kami</div>
+						<br/>
+						Sebagai tim yang profesional dalam bidang jasa pembuatan website, LOGIQUE menyediakan solusi terbaik yang teroptimasi untuk bisnis Anda. Selain jasa pembuatan website, LOGIQUE juga menyediakan jasa lainnya seperti  jasa konsultasi digital, jasa pembuatan aplikasi, desain web, pengembangan web system, jasa SEO dan percetakan. Kami memberikan rancangan terstruktur secara detil baik untuk pembuatan website perusahaan, aplikasi, maupun pembuatan website atau sistem berskala besar dengan kombinasi desain yang terarah dan arsitektur sistem yang baik.<br/>
+						<div class="visible-sm visible-xs"><br/></div>
+					</div>
+					<div class="col-md-4">
+						<div class="title">Hubungi Kami</div>
+						<a href="/hubungi-kami.php" target="_blank"><img src="img/logo_logique_footer.png" class="img-responsive "/></a>
+						Ad Premier Building 19th Floor. <br/>
+						Jalan Tb. Simatupang No. 5 <br/>
+						Ragunan, Ps. Minggu, <br/>
+						Jakarta Selatan, Indonesia 12550 <br/>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<div class="copyright">
+			© 2018 Logique Digital Indonesia. All Rights Reserved.
+		</div>
+		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/slick.min.js"></script>
+		<script type="text/javascript">
+		 	$('.slider-app').slick({
+			  infinite: true,
+			  centerMode: true,
+			  centerPadding: '60px',
+			  slidesToShow: 3,
+			  arrows: false,
+			  dots:true,
+			  responsive: [
+				{
+				  breakpoint: 991,
+				  settings: {
+					centerPadding: '0'
+				  }
+				},
+				{
+				  breakpoint: 767,
+				  settings: {
+					centerPadding: '0',
+					slidesToShow: 1
+				  }
+				}
+			  ]
+			});
+		</script>
+	</body>
+</html>
