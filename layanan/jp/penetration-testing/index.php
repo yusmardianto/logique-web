b68273af596e65d431ef6dd9e6e35fe462734763
Gilbertus Nico <nico@logique.co.id>
2018-03-27 14:19:20 +0700

    add career and pentest JP LP

---
+++ layanan/jp/penetration-testing/index.php
@@ -0,0 +1,327 @@
+﻿<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="LOGIQUE menyediakan layanan Penetration testing untuk meningkatkan keamanan sistem atau aplikasi Anda">
+	<meta name="keywords" content="penetration test, penetration testing, jasa penetration test, jasa penetration testing , logique">
+	<meta name="author" content="Logique Digital Indonesia">
+	<meta property="og:title" content="Penetration Test | Logique Digital Indonesia">
+	<meta property="og:type" content="website">
+	<meta property="og:site_name" content="Logique">
+	<meta property="og:url" content="https://www.logique.co.id/layanan/penetration-testing/">
+	<meta property="og:image" content="https://www.logique.co.id/img/og-image.png">
+	<meta property="og:description" content="LOGIQUE menyediakan layanan Penetration testing untuk lebih meningkatkan keamanan sistem atau aplikasi Anda">
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <title>Penetration Test | Logique Digital Indonesia</title>
+	<link href="/css/bootstrap.min.css" rel="stylesheet">	 
+    <link href="css/style.css" rel="stylesheet">
+	<script src="https://use.fontawesome.com/df558bb4a1.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900" rel="stylesheet">
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-105878648-1', 'auto');
+	ga('send', 'pageview');
+
+	</script>
+  </head>
+<body>
+	
+	<div class="header-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<a href="https://www.logique.co.id"><img class="img-responsive logo" src="img/logo.png" alt="Logique"/></a>
+					<img class="img-responsive center-block top-banner" src="img/top-banner.png" alt="Top Banner"/>
+					<div class="what-is">
+						<div class="title">Webサイトのセキュリティ診断サービス｜WEB脆弱性診断</div>
+						<div class="row">
+							<div class="col-md-offset-1 col-md-11">
+								<p><br/>
+								LOGIQUEのWEBサイトセキュリティ診断サービスは、お客様のWEBサイト、E-Commerce、WEBアプリケーションの脆弱性を診断し、レポート、対策案を提案します。インドネシアではまだまだ企業のWEBセキュリティ意識が低いのが実状です。それに対し、攻撃側、セキュリティホールを突こうとする立場側の数も質もインターナショナルレベルなので、セキュリティインシデントが頻繁に発生しています。問題が起こってからでは遅いです。サイバー攻撃手法やソフトウェア脆弱性の観点からWebサイトのセキュリティをチェックしておく必要があります。<br/>
+								金融機関ではないから、重要な個人情報は扱ってないから、などでセキュリティ診断・対策が必要でないとお考えの企業もありますが、WEBサイトが改ざんされる、踏み台にされ大きな事件に関与してしまうなどのリスクがあります。<br/>
+								簡易なWEBについても、それなりのセキュリティ診断をし自社WEBサイトのセキュリティ脆弱性について把握しておく必要があります。WEBサイトの規模に合わせた様々なセキュリティ診断を柔軟に実施します。手軽にハジメられる簡易な診断メニューも用意しておりますのでご相談ください。
+
+								</p>
+							</div>
+						</div>
+					</div>
+					<br/><br/>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="target-umum-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7">
+					<div class="title fw-300"><span class="yellow">WEBサイトセキュリティ診断</span>内容、把握できる<span class="yellow">WEBのセキュリティリスク</span></div>
+					<br/>
+					<ul>
+						<li><span class="yellow">12項目の診断項目について確認、レポートします。WEBサイトのセキュリティレベルを素早く把握できるWEBサイト脆弱性診断アセスメントサービスです。</span></li>
+						<li><span class="yellow">セキュリティ診断対象WEBサイトの規模（ページ数や検索機能やフォームの有無や数）により診断対象を限定する場合があります。</span></li>
+						<li><span class="yellow">セキュリティ診断ツールを用いた診断を行います。網羅的な確認が可能です。</span></li>
+						<li><span class="yellow">必要に応じてソース解析診断などのマニュアル診断を行います。セキュリティ上の重要事項について潜在的なリスクを発見します。</span></li>
+						<li><span class="yellow">診断結果の報告に加えて改善方法や優先順位付けなどを提言します。</span></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<img src="img/typing.png" width="100%" alt="Typing"/><br/><br/>
+	
+	<div class="plus-minus-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="text-center lh-30">
+						<span class="title fw-300"><b>WEBサイトセキュリティ診断</b>が必要な理由</span><br/>
+						インドネシアにおいても、企業に対して、不正アクセス、WEBサイトの改ざんをはじめとするWEBサーバー、WEBサイトへの攻撃が頻繁に行われています。結果として個人情報漏えい、WEBサイトの改ざん、システムダウンやアクセス障害、踏み台化などの被害が起こっています。<br/>
+						企業のブランド価値や信頼感を伝えるためのお客様とのコミュニケーションチャネルであるWEBサイトを安心してユーザーにご利用いただくために、脆弱性を払拭した安全なWEBサイトを提供することは、企業の義務だと考えます。WEBサイトでセキュリティ事件が発生することでブランド価値が大きい毀損するリスクがあります。<br/>
+						こういったリスクを取り除き、安全なWEBサイトであることを維持し続けるために、WEBサイトのセキュリティ脆弱性診断は欠かすことはできません。
+					</div>	
+					<br/><br/>
+					<div class="text-center lh-30">
+						<span class="title fw-300"><b>WEBサイトセキュリティ脆弱性診断</b>のカバー範囲</span><br/>
+						WEB、WEBサーバーのセキュリティリスクは複雑です。ミドルウェアやインフラのセキュリティ、ネットワークセキュリティ、ソーシャルエンジニアリングのリスクなど広範囲で非常に多くの考えなくてはいけないこと、対策しなくてはいけないことがあります。<br/>
+						ただし、この困難さ、複雑さ、高コストを理由として何もしないのではなく、低コストで短期間で実施できるLOGIQUEのWEBサイトセキュリティ診断を受診されることをお奨めいたします。
+
+					</div>	
+				</div>
+				<!--<div class="clearfix"></div><br/>
+				<div class="col-md-6">
+					<hr/>
+					<div class="each-plus-minus">
+						<div class="title-each"><img src="img/kelebihan.png" alt="Kelebihan"/>KELEBIHAN</div>
+						<ul>
+							<li><span>Dapat dilakukan secara cepat dengan sedikit waktu sehingga harganya murah</span></li>
+							<li><span>Keterampilan yang dibutuhkan untuk melakukan teknik pengujian pentest relative lebih rendah jika dibandingkan dengan melakukan teknik pengujian menggunakan pemeriksaan source code</span></li>
+							<li><span>Pengujian langsung dilakukan pada kode yang digunakan (exposed)</span></li>
+						</ul>
+					</div>
+					<hr/>
+				</div>
+				<div class="col-md-6">
+					<hr/>
+					<div class="each-plus-minus">
+						<div class="title-each"><img src="img/kekurangan.png" alt="Kekurangan"/>KEKURANGAN</div>
+						<ul>
+							<li><span>Terlalu lambat jika dilakukan dalam SDLC</span></li>
+							<li><span>Hanya melakukan pengujian pada dampak depannya saja</span></li>
+						</ul>
+					</div>
+					<hr/>
+				</div>	-->
+			</div>	
+		</div>	
+	</div>	
+	<br/>
+	<div class="mengapa-harus">WEBサイトのセキュリティ脆弱性診断については<a href="https://www.logique.co.id/" target="_blank"><b>LOGIQUE</b></a>にご相談ください。</div>
+	<br/>
+	<div class="metode">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-offset-1 col-md-10">
+					<p>
+					インドネシアにて政府系期間や金融機関、自動車メーカーWEBサイトのセキュリティ診断をしてきました。第一回アセスメント結果は多くのケースでショッキングな結果になることが多いです。但しそこから優先度をつけて対策をしていけばリスクを軽減することができますし、しっかりと定期的な診断を続けていけばリスクはほぼなくなります。
+					</p>
+
+					<!--<div class="title">Metode apa yang digunakan Logique Digital Indonesia dalam melakukan pentest?</div>
+					<center>Dalam melakukan pentest, Logique Digital Indonesia mempunyai 3 metode yang dapat digunakan, antara lain yaitu:</center>
+					<br/>
+					<div class="row row-center">
+						<div class="col-sm-2">
+							<img alt="Black Box" src="img/black-box.png" class="img-responsive center-block"/>
+						</div>
+						<div class="col-sm-10">
+							<div class="subtitle fs-18"><b>Black Box Testing</b></div>
+							<div>
+							Merupakan pengujian yang dilakukan berdasarkan detail aplikasi, seperti tampilan aplikasi, fungsi-fungsi yang terdapat pada aplikasi, serta penyesuaian alur fungsi pada aplikasi dengan bisnis yang diinginkan oleh pelanggan. Pengujian ini dilakukan tanpa melihat dan menguji source code program yang ada pada aplikasi.
+							</div>
+						</div>
+					</div>
+					<hr/>
+					<div class="row row-center">
+						<div class="col-sm-2">
+							<img alt="White Box" src="img/white-box.png" class="img-responsive center-block"/>
+						</div>
+						<div class="col-sm-10">
+							<div class="subtitle fs-18"><b>White Box Testing</b></div>
+							<div>
+							Merupakan pengujian yang dilakukan berdasarkan detail prosedur serta alur logika dari sebuah kode program. Pada metode ini, tester akan melihat keseluruhan source code sebuah program untuk menemukan bugs dari kode program tersebut.
+							</div>
+						</div>
+					</div>
+					<hr/>
+					<div class="row row-center">
+						<div class="col-sm-2">
+							<img alt="Grey Box" src="img/grey-box.png" class="img-responsive center-block"/>
+						</div>
+						<div class="col-sm-10">
+							<div class="subtitle fs-18"><b>Grey Box Testing</b></div>
+							<div>
+							Merupakan metode pengujian yang berasal dari kombinasi Black Box dan White Box. Dimana pentester melakukan pengujian aplikasi berdasarkan spesifikasi namun menggunakan cara kerja dari dalam aplikasi tersebut alias source code program.
+							</div>
+						</div>
+					</div>-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="clearfix"></div><br/><br/>
+	<!--<div class="tahapan-title">
+		<div class="question">
+			Apa saja tahapan-tahapan yang dilakukan <b>Logique</b> dalam melakukan penetration testing?
+		</div>
+		<div class="answer">
+			 <b>Logique Digital Indonesia </b> menggunakan standar penetration testing international sebagai acuan pelaksanaan dalam melakukan pengujian, antara lain yaitu:
+		</div>
+	</div>
+	<br/><br/>
+	<div class="tahapan">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-sm-4">
+							<img alt="Step 1" src="img/step1.png" class="img-responsive center-block"/><br/>
+						</div>
+						<div class="col-sm-8 text-justify">
+							<div class="title"><span>1</span> <b>Pre-engagement Interactions</b></div>
+							Yaitu tahapan dimana pentester Logique akan menjelaskan kegiatan apa saja yang akan dilakukan kepada pelanggan/ client, mulai dari hal pertama yang akan dilakukan hingga tujuan akhir yang akan dicapai dengan melakukan pentest ini.<br/><br/>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-sm-4">
+							<img alt="Step 2" src="img/step2.png" class="img-responsive center-block"/><br/>
+						</div>
+						<div class="col-sm-8 text-justify">
+							<div class="title"><span>2</span> <b>Intelligence Gathering</b></div>
+							Yaitu tahapan pengumpulan data informasi sebanyak-banyaknya mengenai target perusahaan yang akan dicapai dengan menggunakan berbagai metode yang telah ada.<br/><br/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hidden-xs"><br/></div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-sm-4">
+							<img alt="Step 3" src="img/step3.png" class="img-responsive center-block"/><br/>
+						</div>
+						<div class="col-sm-8 text-justify">
+							<div class="title"><span>3</span> <b>Threat Modelling</b></div>
+							Yaitu sebuah tahapan pengujian dimana pentester akan mencari celah keamanan (<i>vulnerabilities</i>) berdasarkan data yang telah dikumpulkan pada tahapan sebelumnya. Di Logique, pada tahapan ini pentester selain mencari celah sekaligus juga menentukan celah apa yang paling efektif untuk digunakan.
+							<br/><br/>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-sm-4">
+							<img alt="Step 4" src="img/step4.png" class="img-responsive center-block"/><br/>
+						</div>
+						<div class="col-sm-8 text-justify">
+							<div class="title"><span>4</span> <b>Vulnerability Analysis</b></div>
+							Yaitu tahapan dimana pentester akan melakukan kombinasi dari informasi yang telah didapatkan mengenai celah keamanan dengan melakukan serangan yang paling efektif ke aplikasi atau sistem.<br/><br/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hidden-xs"><br/></div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-sm-4">
+							<img alt="Step 5" src="img/step5.png" class="img-responsive center-block"/><br/>
+						</div>
+						<div class="col-sm-8 text-justify">
+							<div class="title"><span>5</span> <b>Exploitation</b></div>
+							Yaitu tahapan dimana pentester melakukan penyempurnaan analisis untuk melakukan serangan yang efektif pada target yang telah ditentukan.
+							<br/><br/>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-sm-4">
+							<img alt="Step 6" src="img/step6.png" class="img-responsive center-block"/><br/>
+						</div>
+						<div class="col-sm-8 text-justify">
+							<div class="title"><span>6</span> <b>Post Exploitation</b></div>
+							Yaitu tahapan dimana pentester berhasil masuk ke dalam sistem target, kemudian akan melakukan analisis menyeluruh terhadap infrastruktur yang ada serta menentukan bagian mana yang paling critical untuk perusahaan. Selain itu, di tahap ini pentester juga akan menjelaskan berbagai dampak serangan/ kerugian yang akan terjadi kepada perusahaan.
+							<br/><br/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hidden-xs"><br/></div>
+			<div class="row">
+				<div class="bg-yellow">
+					<div class="row">
+						<div class="col-sm-2">
+							<img alt="Step 6" src="img/report.png" class="img-responsive center-block"/>
+						</div>
+						<div class="col-sm-10 text-justify">
+							<div class="title"><span>7</span> <b>Reporting</b></div>
+							Dalam kegiatan pentest, reporting merupakan hal yang paling penting. Pada tahapan ini, seorang pentester akan membuat laporan mengenai pentesting yang sudah dilakukan, resiko yang mungkin terjadi serta membuat solusi perbaikan sistem.
+						</div>
+					</div>
+				</div>
+			</div>
+			<br/><br/>
+		</div>
+	</div>-->
+	
+	<div class="harga">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-offset-2 col-md-8">
+					<div class="title">WEBサイトの簡易セキュリティ脆弱性診断サービスをお試し価格でご提供しています。</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<img alt="10 juta" src="img/10-juta.png" class="img-responsive center-block"/>
+				</div>
+			</div><br/>
+			<div class="row">
+				<div class="col-md-offset-1 col-md-10">
+					<div class="subtitle">まず簡易診断ではありますが、自社WEBサイトがどのようなセキュリティ状態にあり、どの程度のリスクにさらされているのか把握されることをお奨めいたします。<br/>
+					お気軽にご相談のご連絡をください。日本語でお問い合わせいただけます。</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-offset-3 col-md-6">
+					WEBサイトセキュリティ診断サービスに関する、診断内容、レポート項目、費用、期間など詳細に関してはお気軽に日本語でお問い合わせいただければ幸いです。こちらまで(<a href="mailto:info@logique.co.id">info@logique.co.id</a>）メールお送りいただくか<a href="/jp/contact.php">こちら</a>から問い合わせ送信いただきますようお願いいたします。<br/><br/>
+					<a href="/jp/contact.php" target="_blank"><img alt="Form" src="img/form-footer.png" class="img-responsive center-block"/></a>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+		<br/>
+		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/jquery.matchHeight.js"></script>
+	</body>
+	<script>
+		$(function() {
+			$('.each-plus-minus').matchHeight();
+		});	
+	</script>
+</html>
