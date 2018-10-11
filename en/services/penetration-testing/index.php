36c8da08ae5ea14ae68eaa4525afc134060cd1b1
taufik91 <dion@logique.co.id>
2018-10-11 19:19:28 +0700

    edit penetration test page

--- en/services/penetration-testing/index.php
+++ en/services/penetration-testing/index.php
@@ -4,17 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="description" content="Website security assessment service | WEB vulnerability assessment by PT LOGIQUE Digital Indonesia. We check and report on 12 crucial items of the assessment. It is a website vulnerability assessment service that can quickly scan the security level of your website.">
+    <meta name="description" content="We are providing Website security assessment service, penetration test and WEB vulnerability assessment in Jakarta, Indonesia. It is a website vulnerability assessment service that can quickly scan the security level of your website.">
 	<meta name="keywords" content="penetration test, penetration testing, jasa penetration test, jasa penetration testing , logique">
 	<meta name="author" content="Logique Digital Indonesia">
-	<meta property="og:title" content="Penetration Test | Logique Digital Indonesia">
+	<meta property="og:title" content="WEB Security | Penetration Test | Logique Digital Indonesia">
 	<meta property="og:type" content="website">
 	<meta property="og:site_name" content="Logique">
 	<meta property="og:url" content="https://www.logique.co.id/en/services/penetration-testing/">
 	<meta property="og:image" content="https://www.logique.co.id/img/og-image.png">
-	<meta property="og:description" content="Website security assessment service | WEB vulnerability assessment by PT LOGIQUE Digital Indonesia. We check and report on 12 crucial items of the assessment. It is a website vulnerability assessment service that can quickly scan the security level of your website.">
+	<meta property="og:description" content="We are providing Website security assessment service, penetration test and WEB vulnerability assessment in Jakarta, Indonesia. It is a website vulnerability assessment service that can quickly scan the security level of your website.">
 	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-    <title>Penetration Test | Logique Digital Indonesia</title>
+    <title>WEB Security | Penetration Test | Logique Digital Indonesia</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">	 
     <link href="css/style.css" rel="stylesheet">
     <link href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css" rel="stylesheet" type="text/css" />
@@ -316,7 +316,102 @@
 			</div>
 		</div>
 	</div>
-		
+	<footer>
+	<!--<script type="text/javascript">!function(t,e){t.artibotApi={l:[],t:[],on:function(){this.l.push(arguments)},trigger:function(){this.t.push(arguments)}};var a=!1,i=e.createElement("script");i.async=!0,i.type="text/javascript",i.src="https://app.artibot.ai/loader.js",e.getElementsByTagName("head").item(0).appendChild(i),i.onreadystatechange=i.onload=function(){if(!(a||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState)){new window.ArtiBot({i:"ffce07da-cb7b-4dd0-be97-171190ab7c25"});a=!0}}}(window,document);</script>-->
+	<script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '4c745242-66c8-4931-9b56-3c3bdcc5cbe6', f: true }); done = true; } }; })();</script>
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-105878648-1', 'auto');
+	ga('send', 'pageview');
+
+	</script>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-push-4">
+					<div class="row">
+						<div class="col-sm-3 link-footer-nav">
+							<a href="index.php">Home</a><br/><br/>
+							<a href="about.php">About Us</a><br/><br/>
+							<a href="portfolio.php">Portfolio</a><br/><br/>
+							<a href="services.php">Service</a><br/><br/>
+							<a href="joinus.php">Career</a><br/><br/>
+							<a href="/career/">Join LOGIQUE</a><br/><br/>
+							<a href="contact.php">Contact Us</a><br/>
+						</div>
+						<div class="col-sm-5 link-footer">
+							<div class="footer-title">Our Service</div>
+							<h5><a href="/en/services/mekanisme-pembuatan-website" target="_blank">Web Development</a></h5>
+								<h6><a href="/en/services/pembuatan-website-karir-perusahaan/" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Recruitment Website Creation</a></h6>
+								<h6><a href="/layanan/pembuatan-website-restoran/" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Restaurant Website Creation</a></h6>
+								<h6><a href="/layanan/pembuatan-website-klinik-gigi/" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Dentist Website Creation</a></h6>
+							<h5><a href="/en/services/pembuatan-sistem-web/" target="_blank">Web System Development</a></h5>
+							<h5><a href="/en/services/pembuatan-aplikasi-mobile/">Mobile App Development</a></h5>
+							<h5><a href="/en/services/penetration-testing/">WEB Security Assesment (VA Test)</a></h5>
+						</div>
+						<div class="col-sm-4 link-footer">
+							<div class="footer-title">Our Product</div>
+							<h5 style="margin-bottom: 10px;">
+								<a href="/en/product/dokodemo-kerja.php" target="_blank">
+									Dokodemo-Kerja<br/><div style="font-size: 12px;">(Remote Working Tool)</div>
+								</a>
+							</h5>
+							<h5 style="margin-bottom: 10px;">
+								<a href="/en/product/nippo.php" target="_blank">
+									Nippo<br/><div style="font-size: 12px;">(Daily Report Tool)</div>
+								</a>
+							</h5>
+							<h5>
+								<a href="/en/product/sugoi-saiyo.php" target="_blank">
+									Sugoi Saiyo<br/><div style="font-size: 12px;">(Employee Recruitment System)</div>
+								</a>
+							</h5>
+							<div class="hidden-xs"><br/></div>
+							<div class="footer-title">Social Media</div>
+							<div class="row medsos-new">
+								<div class="col-xs-3 text-center">
+									<a href="https://www.facebook.com/logique.id/" target="_blank"><i class="fa fa-facebook-official"></i></a>
+								</div>
+								<div class="col-xs-3 text-center">
+									<a href="https://twitter.com/LogiqueDigital" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+								</div>
+								<div class="col-xs-3 text-center">
+									<a href="https://www.instagram.com/logiquedigital/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+								</div>
+								<div class="col-xs-3 text-center">
+									<a href="https://www.linkedin.com/company/13420656" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br/>
+				</div>
+				<div class="col-sm-4 col-sm-pull-8">
+				<div itemscope itemtype="http://schema.org/Organization">
+					<div class="footer-title"><span itemprop="name">LOGIQUE DIGITAL INDONESIA</span></div>
+					<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+					<span itemprop="streetAddress"> Ad Premier Building 19th Floor. Jalan Tb. Simatupang No. 5 <br/>
+					Ragunan, Ps. Minggu,  Jakarta Selatan, Indonesia </span><span itemprop="postalCode">12550</span>
+					<br><br>
+					&nbsp;&nbsp;&nbsp;<i class="fa fa-phone"></i>&nbsp;&nbsp;&nbsp;(021) 22708935 / 36 
+					<br>
+					<a href="https://api.whatsapp.com/send?phone=0811870321" target="_blank">&nbsp;&nbsp;&nbsp;<i class="fa fa-mobile" style="font-size: 18px;vertical-align: -1px;"></i>&nbsp;&nbsp;<i class="fa fa-whatsapp"></i>&nbsp;&nbsp;&nbsp;0811-870-321</a>
+					<br><br>
+					</div>
+				</div>
+					LOGIQUE provides the best solution, optimised for your business, as a professional team in web consulting, website creation, Smartphone website creation, responsive web design creation, system development, and graphic creation. We develop structured creative websites thoughtfully from large-scale corporate websites to smartphone websites with the combination of art direction and information architecture design.				
+					<br/><br/>
+					<div itemscope itemtype="http://schema.org/CreativeWork">
+						<div class="copyright">&copy; <span itemprop="copyrightYear">2017</span>  
+						<span itemprop="copyrightHolder" itemtype="http://schema.org/Organization"><span itemprop="name">Logique Digital Indonesia. All Rights Reserved.</span></span></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
 		<br/>
 		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
