3aea50d7f2a84a061826016e4abc9026a3a2749c
Gilbertus Nico <nico@logique.co.id>
2018-10-29 17:38:23 +0700

    update portfolio

--- portofolio.php
+++ portofolio.php
@@ -550,6 +550,17 @@
 									<br><div class="grey-clr">PWA, Mobile, E-Commerce</div><br><br><br>
 								</div>
 							</div>
+							<div class="clearfix"></div>
+							<div class="col-sm-4">								
+								<a href="https://cikarang-japanese-school.com/" target="_blank">
+									<img src="/img/portfolio/cjs.png" class="img-responsive center-block">
+									Cikarang Japanese School
+								</a>
+								<div class="addition-desc text-left">
+									<br>WEB Dev & Operation (Design | Coding | Wordpress | AWS Operation)<br>
+									<br><div class="grey-clr">Japanese WEB, Blog, Mobile, Application Form, School</div><br><br><br>
+								</div>
+							</div>
 						</div>
 						<br/>
 						<span class="characteristic-title">Pengalaman kami dalam pengembangan sistem web</span>
@@ -812,6 +823,11 @@
 							<div class="col-sm-4">
 								<img src="/img/portfolio/otomart-app.png" class="img-responsive center-block">
 								Otomart -  Dealer Car App<br>
+							</div>
+							<div class="clearfix"></div>
+							<div class="col-sm-4">
+								<img src="/img/portfolio/jba-bid.png" class="img-responsive center-block">
+								JBA Indonesia Auction Bidding App<br>
 							</div>
 						</div>
 					<br/>
