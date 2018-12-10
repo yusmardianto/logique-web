aec3d6485254c2873d4d27581fa219ad0129e1c2
taufik91 <dion@logique.co.id>
2018-12-10 16:13:40 +0700

    case study page

---
+++ case-study/index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" itemprop="description" content="Jasa pembuatan website dan SEO berkualitas di Jakarta, konsultasikan kebutuhan digital Anda melalui LOGIQUE, Web Developer asal Jepang no. 1 di Indonesia.">
+	<meta name="keywords" content="jasa pembuatan website, jasa pembuatan aplikasi, jasa seo, jasa promosi online, digital marketing, web developer, desain web, logique">
+	<meta name="author" content="PT. Logique Digital Indonesia">
+	<meta itemprop="name" content="PT. Logique Digital Indonesia">
+	<meta property="og:title" content="Jasa Pembuatan Website dan SEO | LOGIQUE Digital Indonesia">
+	<meta property="og:type" content="website">
+	<meta property="og:site_name" content="Logique">
+	<meta property="og:url" content="https://www.logique.co.id/">
+	<meta property="og:image" content="https://www.logique.co.id/img/og-image.png">
+	<meta property="og:description" content="Jasa pembuatan website dan SEO berkualitas di Jakarta, konsultasikan kebutuhan digital Anda melalui LOGIQUE, Web Developer asal Jepang no. 1 di Indonesia.">
+	<link rel="amphtml" href="https://www.logique.co.id/amp/index.html">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-105878648-1', 'auto');
+	ga('send', 'pageview');
+
+	</script>
+    <title>Jasa Pembuatan Website dan SEO | LOGIQUE Digital Indonesia</title>
+
+	<link href="../css/bootstrap.min.css" rel="stylesheet">	
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
+	</noscript>
+	
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+	<link href="../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    
+    </head>
+	<body>
+		<div class="wrapper o-wrapper top-only">
+		
+			<!-- NAVIGATION -->
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar first"></span>
+				<span class="icon-bar second"></span>
+				<span class="icon-bar third"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="myNavbar">
+			<div class="backdrop-test"></div>
+				<div class="logique-nav">
+					<div class="margin-for-nav">
+						<div class="text-center active">
+							<a href="#">
+								<div class="img-home"></div>
+								Beranda
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="tentang-kami.php">
+								<div class="img-about"></div>
+								Tentang Kami
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="portofolio.php">
+								<div class="img-portfolio"></div>
+								Portfolio
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="layanan.php">
+								<div class="img-services"></div>
+								Layanan
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="karir.php">
+								<div class="img-career"></div>
+								Karir
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="hubungi-kami.php">
+								<div class="img-contact"></div>
+								Hubungi Kami
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="https://www.logique.co.id/blog/" target="_blank">
+								<div class="img-blog"></div>
+								Blog
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- NAVIGATION -->
+			
+			<div class="container-fluid">
+				<div class="row bg-atas-log-1">
+					<div class="language-btn">
+						<ul class="list-inline">
+							<li class="active"><a href="#">EN</a></li>
+							<li><a href="/layanan/pembuatan-sistem-web/">ID</a></li>
+							<li><a href="/jp/services/pembuatan-sistem-web/">JP</a></li>
+						</ul>
+					</div>
+					<div class="col-md-12 text-right">
+						<h1 class="title-JBA">JBA Auction System</h1>
+					</div> 
+
+				</div>
+				<div class="row bg-atas-log-2">
+
+					<div class="row col-md-12 set-first">
+						<div class="col-md-6 text-left text-pl8">
+							<h1 class="h1-desc">JBA Indonesia</h1>
+							<p class="p-desc">JBA Indonesia adalah perusahaan yang bergerak di bidang jasa LELANG, khususnya untuk produk otomotif seperti MOBIL dan MOTOR. PT. JBA Indonesia salah satu perusahaan asing di bidang LELANG yang berskala besar berkantor pusat di Jepang, mulai berdiri di Indonesia pada tanggal 16 November 2011, dan telah meraih penghargaan sebagai balai LELANG di tingkat inovasi terbaik di tahun 2013 dan 2014. Terpercaya, Aman, Praktis dan Nyaman merupakan motto kami dalam memberikan pelayanan secara professional dan menciptakan kepuasan mitra kerja dan pelanggan.</p>
+						</div>
+						<div class="col-md-6 text-center pr-ikon">
+							<img src="./img/ikon-2.png" class="ikon" alt="test">
+						</div>
+					</div>
+
+					<div class="row col-md-12 set-first">
+						<div class="col-md-6 text-center pr-ikon">
+							<img src="./img/ikon-1.png" class="ikon" alt="test">
+						</div> 
+						<div class="col-md-6 text-left text-pl2">
+							<h1 class="h1-desc">Latar Belakang</h1>
+							<p class="p-desc">Disamping itu, kami juga mendukung kebutuhan dari setiap pelanggan kami yang memiliki aktifitas terikat dengan bisnis mereka, dengan membangun sebuah sistem yang terpercaya, akurat dan real-time. Para pelanggan kami bisa mencari informasi secara lengkap dan terperinci melalui website dalam pemilihan unit dengan harga yang sangat competitive dan gambar fisik dari setiap unit yang hendak dilelang, dengan website yang userfriendly sehingga berdasarkan data sistem, 60% pengunjung website kami bisa mengakses melalui smartphone.</p>
+							<button class="btn btn-round">Details</button>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="row no-bg">
+					<div class="col-md-12">
+						<div class="col-md-6 text-left text-pl2">
+							<h1>Teknologi yang digunakan</h1>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+						</div>
+						<div class="col-md-6 text-center">
+							<img src="./img/ikon-3.png" class="ikon-coding" alt="test">
+						</div>
+					</div>
+				</div>
+
+				<div class="row bg-atas-log-3">
+					<div class="col-md-12 p12em">
+						<div class="text-center">
+							<h1>JBA Auction System</h1>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been<br>the industry's standard dummy text ever since the 1500,.</p>
+							<img src="./img/ikon-4.png" class="ikon4" alt="test">
+						</div>
+					</div>
+				</div>
+
+				<div class="row bg-atas-log-4">
+					<div class="col-md-12 p2em">
+						<div class="text-center">
+							<h1>Tentang <img src="./img/logo.png" class="logo" alt="test"></h1>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been<br>the industry's standard dummy text ever since the 1500,.</p>
+						</div>
+					</div>
+					<div class="row row-centered">
+						<button class="btn btn-round-group col-xs-4 col-centered">Kontak Kami</button>
+						<button class="btn btn-round-group col-xs-4 col-centered">Layanan Kami</button>
+						<button class="btn btn-round-group col-xs-4 col-centered">Portfolio</button>
+						<button class="btn btn-round-group col-xs-4 col-centered">Ingin Buat Website?</button>
+						<button class="btn btn-round-group col-xs-4 col-centered">Ingin Buat System?</button>
+					</div>
+				</div>
+			</div>
+			<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<div><h2>About Us</h2></div>
+						<br/>
+						As a professional team in website creation services, LOGIQUE provides the best optimized solution for your business. In addition to website creation services, LOGIQUE also provides other services such as digital consulting services, application creation services, web design, web system development, SEO services, and printing. We provide detailed structured designs for the creation of corporate websites, applications, as well as the creation of large-scale websites or systems with a combination of targeted design and good system architecture.<br/>
+						<div class="visible-sm visible-xs"><br/></div>
+					</div>
+					<div class="col-md-4 contact-us-res">
+						<div><h2>Contact Us</h2></div>
+						<a href="/en/contact.php" target="_blank"><img alt="Logo" src="./img/logo.png" class="logo"/></a> <br />
+						Ad Premier Building 19th Floor. <br/>
+						Jalan Tb. Simatupang No. 5 <br/>
+						Ragunan, Ps. Minggu, <br/>
+						Jakarta Selatan, Indonesia 12550 <br/>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<div class="copyright">
+			© 2018 Logique Digital Indonesia. All Rights Reserved.
+		</div>
+		</div>
+		<script type="application/ld+json">
+			{
+			  "@context": "http://schema.org",
+			  "@type": "Organization",
+			  "address": {
+					"@type": "PostalAddress",
+					"addressLocality": "Jakarta, Indonesia",
+					"postalCode": "12550",
+					"streetAddress": "Ad Premier Building 19th Floor. Jalan Tb. Simatupang No. 5, Ragunan, Ps. Minggu,  Jakarta Selatan"
+			  },
+			  "logo": "https://www.logique.co.id/img/logo.png",
+			  "email": "info(at)logique.co.id",
+			  "telephone": "+62 (21) 2270 8935/36"
+			}
+		</script>
+	</body>
+</html>
