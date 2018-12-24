e4cc31ecc7d03a1b92bd26230157d4ffc98f7fe9
taufik <taufik@logique.co.id>
2018-12-24 10:58:50 +0700

    update case study (jba auction system)

--- case-study/jba-auction-system/index.php
+++ case-study/jba-auction-system/index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="id">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="description" itemprop="description" content="Jasa pembuatan website dan SEO berkualitas di Jakarta, konsultasikan kebutuhan digital Anda melalui LOGIQUE, Web Developer asal Jepang no. 1 di Indonesia.">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <meta name="description" itemprop="description" content="...">
 	<meta name="keywords" content="jasa pembuatan website, jasa pembuatan aplikasi, jasa seo, jasa promosi online, digital marketing, web developer, desain web, logique">
 	<meta name="author" content="PT. Logique Digital Indonesia">
 	<meta itemprop="name" content="PT. Logique Digital Indonesia">
-	<meta property="og:title" content="Jasa Pembuatan Website dan SEO | LOGIQUE Digital Indonesia">
+	<meta property="og:title" content="Case Study - JBA Auction System | LOGIQUE Digital Indonesia">
 	<meta property="og:type" content="website">
 	<meta property="og:site_name" content="Logique">
-	<meta property="og:url" content="https://www.logique.co.id/">
+	<meta property="og:url" content="https://www.logique.co.id/case-study/jba-auction-system/">
 	<meta property="og:image" content="https://www.logique.co.id/img/og-image.png">
-	<meta property="og:description" content="Jasa pembuatan website dan SEO berkualitas di Jakarta, konsultasikan kebutuhan digital Anda melalui LOGIQUE, Web Developer asal Jepang no. 1 di Indonesia.">
+	<meta property="og:description" content="...">
 	<link rel="amphtml" href="https://www.logique.co.id/amp/index.html">
 	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-    <script>
+  <script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -26,26 +26,42 @@
 	ga('send', 'pageview');
 
 	</script>
-    <title>Jasa Pembuatan Website dan SEO | LOGIQUE Digital Indonesia</title>
+  <title>Case Study - JBA Auction System | LOGIQUE Digital Indonesia</title>
 
 	<link href="../../css/bootstrap.min.css" rel="stylesheet">	
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
 	</noscript>
 	
-    <link href="../../css/style.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+  <link href="../../css/style.css" rel="stylesheet">
 	<link href="../../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <!-- case study style -->
+  <link href="css/style.css" rel="stylesheet">
     
     </head>
 	<body>
 		<div class="wrapper o-wrapper top-only">
+
+      <header>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-6">
+              <img src="../../img/logo.png" alt="LOGIQUE"><span>STUDI KASUS</span>
+            </div>
+            <div class="col-xs-6 text-right">
+              <ul>
+                <li><a href="#">EN</a></li>
+                <li class="active"><a href="#">ID</a></li>
+                <li><a href="#">JP</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
 		
 			<!-- NAVIGATION -->
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
 				<span class="icon-bar first"></span>
 				<span class="icon-bar second"></span>
-				<span class="icon-bar third"></span>
+				<span class="icon-bar third"></span> 
 			</button>
 			<div class="collapse navbar-collapse" id="myNavbar">
 			<div class="backdrop-test"></div>
@@ -96,117 +112,108 @@
 					</div>
 				</div>
 			</div>
-			<!-- NAVIGATION -->
-			
-			<div class="container-fluid">
-				<div class="header">
-					<div class="language-btn">
-						<div class="text-left row-header">
-							<img src="img/logo.png" class="logo-atas" alt="logique"> STUDI KASUS
-						</div>
-						<ul class="list-inline text-right">
-							<li><a href="#">EN</a></li>
-							<li class="active"><a href="#">ID</a></li>
-							<li><a href="/#">JP</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="row bg-atas-log-1">
-					<div class="row col-md-12 first-div">
-						<div class="col-md-6 col-sm-6 text-left">
-							<img src="img/ikon-atas.png" class="icon-top" alt="ikon-atas">
-						</div> 
-						<div class="col-md-6 col-sm-6 text-center">
-							<h1 class="top-title">JBA Auction System</h1>
-						</div>
-					</div>
-				</div>
-				<img src="img/line/line1.png" class="first-line lines" alt="first-line">
-				<div class="row section-2">
-					<div class="row col-md-12 set-first">
-						<div class="col-md-7 col-sm-7 text-left text-padding-left">
-							<h2 class="subtitle">JBA Indonesia</h2>
-							<p class="p-desc">JBA Indonesia adalah perusahaan yang bergerak di bidang jasa LELANG, khususnya untuk produk otomotif seperti MOBIL dan MOTOR. PT. JBA Indonesia salah satu perusahaan asing di bidang LELANG yang berskala besar berkantor pusat di Jepang, mulai berdiri di Indonesia pada tanggal 16 November 2011, dan telah meraih penghargaan sebagai balai LELANG di tingkat inovasi terbaik di tahun 2013 dan 2014. Terpercaya, Aman, Praktis dan Nyaman merupakan motto kami dalam memberikan pelayanan secara professional dan menciptakan kepuasan mitra kerja dan pelanggan.</p>
-						</div>
-						<div class="col-md-5 col-sm-5 text-center pr-ikon">
-							<img src="img/ikon-2.png" class="ikon" alt="icon">
-						</div>
-					</div>
-				</div>
-				<img src="img/line/line2.png" class="second-line lines" alt="second-line">
-				<div class="row col-md-12 set-first">
-					<div class="col-md-6 col-sm-5 text-right pr-ikon">
-						<img src="img/ikon-1.png" class="ikon" alt="test">
-					</div> 
-					<div class="col-md-6 col-sm-7 text-left text-pl2">
-						<h2 class="subtitle">Latar Belakang</h2>
-						<p class="p-desc">Disamping itu, kami juga mendukung kebutuhan dari setiap pelanggan kami yang memiliki aktifitas terikat dengan bisnis mereka, dengan membangun sebuah sistem yang terpercaya, akurat dan real-time. Para pelanggan kami bisa mencari informasi secara lengkap dan terperinci melalui website dalam pemilihan unit dengan harga yang sangat competitive dan gambar fisik dari setiap unit yang hendak dilelang, dengan website yang userfriendly sehingga berdasarkan data sistem, 60% pengunjung website kami bisa mengakses melalui smartphone.</p>
-						<a href="#" class="btn btn-round">Details</a>
-					</div>
-				</div>
-				<img src="img/list-kanan.png" class="list-kanan" alt="list-kanan">
-				<img src="img/line/line3.png" class="third-line lines" alt="third-line">
-				<div class="row no-bg">
-					<div class="col-md-12">
-						<div class="col-md-6 col-sm-7 text-left text-padding-left-2">
-							<h2 class="subtitle">Teknologi yang digunakan</h2>
-							<p class="p-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-						</div>
-						<div class="col-md-6 col-sm-5 text-center">
-							<img src="img/ikon-3.png" class="ikon-coding" alt="test">
-						</div>
-					</div>
-				</div>
-				<img src="img/line/line4.png" class="fourth-line lines" alt="fourth-line">
-				<div class="row bg-atas-log-3">
-					<div class="col-md-12 p12em">
-						<div class="text-center">
-							<h2 class="subtitle">JBA Auction System</h2>
-							<p class="p-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been<br>the industry's standard dummy text ever since the 1500,.</p>
-							<img src="img/ikon-4.png" class="ikon4" alt="test">
-						</div>
-					</div>
-				</div>
-				<img src="img/list-kiri.png" class="list-kiri" alt="list-kiri">
-				<div class="row bg-atas-log-4">
-					<div class="col-md-12 p2em">
-						<div class="text-center">
-							<h2 class="subtitle">Tentang <img src="img/logo.png" class="logo" alt="test"></h2>
-							<p class="p-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been<br>the industry's standard dummy text ever since the 1500,.</p>
-						</div>
-					</div>
-					<div class="row row-centered">
-						<button class="btn btn-round-group col-xs-3 col-centered">Kontak Kami</button>
-						<button class="btn btn-round-group col-xs-3 col-centered">Layanan Kami</button>
-						<button class="btn btn-round-group col-xs-3 col-centered">Portfolio</button>
-						<button class="btn btn-round-group col-xs-3 col-centered">Ingin Buat Website?</button>
-						<button class="btn btn-round-group col-xs-3 col-centered">Ingin Buat System?</button>
-					</div>
-				</div>
-			</div>
-			<footer>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8">
-						<div><h2>About Us</h2></div>
-						<br/>
-						As a professional team in website creation services, LOGIQUE provides the best optimized solution for your business. In addition to website creation services, LOGIQUE also provides other services such as digital consulting services, application creation services, web design, web system development, SEO services, and printing. We provide detailed structured designs for the creation of corporate websites, applications, as well as the creation of large-scale websites or systems with a combination of targeted design and good system architecture.<br/>
-						<div class="visible-sm visible-xs"><br/></div>
-					</div>
-					<div class="col-md-4 contact-us-res">
-						<div><h2>Contact Us</h2></div>
-						<a href="/en/contact.php" target="_blank"><img alt="Logo" src="img/logo.png" class="logo"/></a> <br />
-						Ad Premier Building 19th Floor. <br/>
-						Jalan Tb. Simatupang No. 5 <br/>
-						Ragunan, Ps. Minggu, <br/>
-						Jakarta Selatan, Indonesia 12550 <br/>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<div class="copyright">
-			© 2018 Logique Digital Indonesia. All Rights Reserved.
-		</div>
+      <!-- NAVIGATION -->
+      
+      <div class="cs-wrap">
+        <section class="cs-top">
+          <img src="img/bg-top.png" alt="case study logique" class="img-fluid">
+          <h1 class="cs-title">JBA Auction System</h1>
+        </section>
+        <section class="cs-section">
+          <img src="img/line-1.png" alt="case study logique" class="cs-line cs-line-1">
+          <div class="row">
+            <div class="col-sm-6 col-md-push-6 relative">
+              <img src="img/img-1.png" alt="case study logique" class="cs-img cs-img-1">
+            </div>
+            <div class="col-sm-6 col-md-pull-6">
+              <h2 class="cs-subtitle">JBA Indonesia</h2>
+              <p class="cs-desc">
+                JBA Indonesia adalah perusahaan yang bergerak di bidang jasa LELANG, khususnya untuk produk otomotif seperti MOBIL dan MOTOR. PT. JBA Indonesia salah satu perusahaan asing di bidang LELANG yang berskala besar berkantor pusat di Jepang, mulai berdiri di Indonesia pada tanggal 16 November 2011, dan telah meraih penghargaan sebagai balai LELANG di tingkat inovasi terbaik di tahun 2013 dan 2014. Terpercaya, Aman, Praktis dan Nyaman merupakan motto kami dalam memberikan pelayanan secara professional dan menciptakan kepuasan mitra kerja dan pelanggan.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section class="cs-section">
+          <img src="img/line-2.png" alt="case study logique" class="cs-line cs-line-2">
+          <div class="row">
+            <div class="col-sm-6 relative">
+              <img src="img/img-2.png" alt="case study logique" class="cs-img cs-img-2">
+            </div>
+            <div class="col-sm-6">
+              <h2 class="cs-subtitle">Latar Belakang</h2>
+              <p class="cs-desc">
+              Disamping itu, kami juga mendukung kebutuhan dari setiap pelanggan kami yang memiliki aktifitas terikat dengan bisnis mereka, dengan membangun sebuah sistem yang terpercaya, akurat dan real-time. Para pelanggan kami bisa mencari informasi secara lengkap dan terperinci melalui website dalam pemilihan unit dengan harga yang sangat competitive dan gambar fisik dari setiap unit yang hendak dilelang, dengan website yang userfriendly sehingga berdasarkan data sistem, 60% pengunjung website kami bisa mengakses melalui smartphone.
+              <br><br>
+              </p>
+              <a href="#" class="btn btn-cs">Detail</a>
+            </div>
+          </div>
+          <img src="img/orm-1.png" alt="case study logique" class="cs-orm cs-orm-1">
+        </section>
+        <section class="cs-section">
+          <img src="img/line-3.png" alt="case study logique" class="cs-line cs-line-3">
+          <div class="row">
+            <div class="col-sm-6 col-md-push-6 relative">
+              <img src="img/img-3.png" alt="case study logique" class="cs-img cs-img-3">
+            </div>
+            <div class="col-sm-6 col-md-pull-6">
+              <h2 class="cs-subtitle">Teknologi yang digunakan</h2>
+              <p class="cs-desc">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section class="cs-section">
+          <img src="img/line-4.png" alt="case study logique" class="cs-line cs-line-4">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="cs-subtitle text-center">JBA Auction System</h2>
+              <p class="cs-desc text-center">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been t
+              <br>he industry's standard dummy text ever since the 1500,.
+              </p>
+              <img src="img/img-4.png" alt="case study logique" class="img-responsive center-block">
+            </div>
+          </div>
+          <img src="img/orm-2.png" alt="case study logique" class="cs-orm cs-orm-2">
+        </section>
+        <section class="cs-bottom">
+          <h2 class="cs-subtitle">Tentang <img src="/img/logo.png" alt="logo logique" class="cs-logo-bottom"></h2>
+          <p class="cs-desc">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been <br> the industry's standard dummy text ever since the 1500,.
+          </p>
+          <a href="#" class="btn btn-cs-bottom">Kontak Kami</a>
+          <a href="#" class="btn btn-cs-bottom">Layanan Kami</a>
+          <a href="#" class="btn btn-cs-bottom">Portfolio</a>
+          <a href="#" class="btn btn-cs-bottom">Ingin buat Website?</a>
+          <a href="#" class="btn btn-cs-bottom">Ingin buat Web System?</a>
+        </section>
+        <footer>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-8">
+                <h2>Tentang Kami</h2>
+                <br/>
+                As a professional team in website creation services, LOGIQUE provides the best optimized solution for your business. In addition to website creation services, LOGIQUE also provides other services such as digital consulting services, application creation services, web design, web system development, SEO services, and printing. We provide detailed structured designs for the creation of corporate websites, applications, as well as the creation of large-scale websites or systems with a combination of targeted design and good system architecture.<br/>
+                <div class="visible-sm visible-xs"><br/></div>
+              </div>
+              <div class="col-md-4">
+                <h2>Hubungi Kami</h2>
+                <a href="/en/contact.php" target="_blank"><img alt="Logo" src="../../img/logo.png" class="logo"/></a> <br />
+                Ad Premier Building 19th Floor. <br/>
+                Jalan Tb. Simatupang No. 5 <br/>
+                Ragunan, Ps. Minggu, <br/>
+                Jakarta Selatan, Indonesia 12550 <br/>
+              </div>
+            </div>
+          </div>
+        </footer>
+        <div class="copyright">
+          © 2018 Logique Digital Indonesia. All Rights Reserved.
+        </div>
+      </div>
+      <?php include '../../footer.php';?>
 		</div>
 		<script type="application/ld+json">
 			{
