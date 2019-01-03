2f5601e60721b5541c077f6b78c2c0bb8137ed18
fahmi <fahmi@logique.co.id>
2019-01-03 16:28:44 +0700

    create opuspark and caroline

---
+++ en/case-study/opuspark/index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <meta name="description" itemprop="description" content="VIP Plaza is an online store of fashion and beauty in Indonesia that continues to develop every day. This fashion online store offers more than 30,000 products from 800 favorite local brands and well-known international variety.">
+	<meta name="keywords" content="jasa pembuatan website, jasa pembuatan aplikasi, jasa seo, jasa promosi online, digital marketing, web developer, desain web, logique">
+	<meta name="author" content="PT. Logique Digital Indonesia">
+	<meta itemprop="name" content="PT. Logique Digital Indonesia">
+	<meta property="og:title" content="Case Study - VIP Plaza Progressive Web App | LOGIQUE Digital Indonesia">
+	<meta property="og:type" content="website">
+	<meta property="og:site_name" content="Logique">
+	<meta property="og:url" content="https://www.logique.co.id/case-study/jba-auction-system/">
+	<meta property="og:image" content="https://www.logique.co.id/img/og-image.png">
+	<meta property="og:description" content="VIP Plaza is an online store of fashion and beauty in Indonesia that continues to develop every day. This fashion online store offers more than 30,000 products from 800 favorite local brands and well-known international variety.">
+	<link rel="amphtml" href="https://www.logique.co.id/amp/index.html">
+	<link rel="shortcut icon" type="image/x-icon" href="../../../favicon.ico">
+  <script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-105878648-1', 'auto');
+	ga('send', 'pageview');
+
+	</script>
+  <title>Case Study - Opus Park | LOGIQUE Digital Indonesia</title>
+
+	<link href="../../../css/bootstrap.min.css" rel="stylesheet">	
+	</noscript>
+	
+  <link href="../../../css/style.css" rel="stylesheet">
+	<link href="../../../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <!-- case study style -->
+  <link href="css/style.css" rel="stylesheet">
+    
+    </head>
+	<body>
+		<div class="wrapper o-wrapper top-only">
+
+      <header>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-6">
+              <img src="../../../img/logo.png" alt="LOGIQUE"><span>CASE STUDY</span>
+            </div>
+            <div class="col-xs-6 text-right">
+              <ul>
+                <li class="active"><a href="#">EN</a></li>
+                <li><a href="#">ID</a></li>
+                <li><a href="#">JP</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+		
+			<!-- NAVIGATION -->
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar first"></span>
+				<span class="icon-bar second"></span>
+				<span class="icon-bar third"></span> 
+			</button>
+			<div class="collapse navbar-collapse" id="myNavbar">
+			<div class="backdrop-test"></div>
+				<div class="logique-nav">
+					<div class="margin-for-nav">
+						<div class="text-center">
+							<a href="/en">
+								<div class="img-home"></div>
+								Home
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/about.php">
+								<div class="img-about"></div>
+								About Us
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/portfolio.php">
+								<div class="img-portfolio"></div>
+								Portfolio
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/services.php">
+								<div class="img-services"></div>
+								Services
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/joinus.php">
+								<div class="img-career"></div>
+								Career
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/contact.php">
+								<div class="img-contact"></div>
+								Contact Us
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="https://www.logique.co.id/blog/" target="_blank">
+								<div class="img-blog"></div>
+								Blog
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+      <!-- NAVIGATION -->
+      
+      <div class="cs-wrap">
+        <section class="cs-top">
+          <img src="img/bg-top.png" alt="case study logique" class="img-fluid">
+          <h1 class="cs-title">Opus Park <p class="hidden-sm hidden-md hidden-lg"><small>Increasing Sales of Units Through Targeted Conversions</small></p></h1>
+          <h2 class="cs-title-2 hidden-xs">Increasing Sales of Units<br>Through Targeted Conversions</h2>
+        </section>
+        <section class="cs-section">
+          <img src="img/line-1.png" alt="case study logique" class="cs-line cs-line-1">
+          <div class="row">
+            <div class="col-sm-6 col-md-push-6 relative">
+              <img src="img/img-1.png" alt="case study logique" class="cs-img cs-img-1">
+            </div>
+            <div class="col-sm-6 col-md-pull-6">
+              <p style="font-size: 22px;font-weight: bold;">Opus Park</p>
+              <h2 class="cs-subtitle">World Class Property in Sentul City</h2>
+              <p class="cs-desc">
+                Opus Park is an iconic suburban residential property developed by PT Izumi Sentul Realty, a Joint Venture (JV) company that consists of a leading property developer in Indonesia, Sentul City, and two well-known Japanese infrastructure companies, namely Sumitomo Corporation and Hankyu Hanshin Properties Corp. Opus Park consists of three premium quality apartment towers with 1,095 units in total. Located in Central Business District (CBD) area of ​​Sentul City, Bogor, Opus Park offers strategic housings in the form of luxury apartments and penthouses with world-class facilities. In the area, also built a shopping center from Japan, AEON Mall, offices, and five-star hotels with a total building area of ​​500 thousand square meters.
+            </div>
+          </div>
+        </section>
+        <section class="cs-section">
+          <img src="img/line-2.png" alt="case study logique" class="cs-line cs-line-2">
+          <div class="row">
+            <div class="col-sm-6 relative">
+              <img src="img/img-2.png" alt="case study logique" class="cs-img cs-img-2 custom1">
+            </div>
+            <div class="col-sm-6">
+              <p style="font-size: 22px;font-weight: bold;">Targets Opus Park Wants to Achieve</p>
+              <h2 class="cs-subtitle">Find the right people</h2>
+              <p class="cs-desc">
+              Opus Park sees the online digital world as the largest potential source of sales of their units. They want targeted marketing through digital marketing to find potential buyers. By using the operation of online advertising and the application of SEO on the website, they have at least two goals. First, they want the right people to contact them—either by telephone, WhatsApp, or email—to ask related questions, and register a schedule to visit the Show Unit with the sales team of Opus Park. Second, so that the number of callers/users increases and they actually meet the schedule to visit the Show Unit.
+              <br>
+              </p>
+            </div>
+          </div>
+          <img src="img/orm-1.png" alt="case study logique" class="cs-orm cs-orm-1">
+        </section>
+        <section class="cs-section">
+          <img src="img/line-3.png" alt="case study logique" class="cs-line cs-line-3">
+          <div class="row">
+            <div class="col-sm-6 col-md-push-6 relative">
+              <img src="img/img-3.png" alt="case study logique" class="cs-img cs-img-3 custom2">
+            </div>
+            <div class="col-sm-6 col-md-pull-6">
+              <p style="font-size: 22px;font-weight: bold;">The Strategy Applied by LOGIQUE</p>
+              <h2 class="cs-subtitle">Maximizing Digital Marketing</h2>
+              <p class="cs-desc custom4">
+              Opus Park wants the operation of ads and social media to get paid traffic, and the implementation of SEO on the website for organic traffic from search engines. Both types of traffic play a major role in achieving the desired goals.
+              <br><br>
+              <a class="btn btn-cs" data-toggle="modal" href='#modal-id'>Detail</a>
+              <div class="modal fade" id="modal-id">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title">Maximizing Digital Marketing</h4>
+                    </div>
+                    <div class="modal-body">
+                    Opus Park wants the operation of ads and social media to get paid traffic, and the
+                    implementation of SEO on the website for organic traffic from search engines. Both types of
+                    traffic play a major role in achieving the desired goals. <br>
+                    For this reason, LOGIQUE applies a phased digital marketing strategy. First, increasing brand
+                    awareness among residential enthusiasts in the form of apartments and penthouses. Second,
+                    make the audience eager to find out more about Opus Park units by visiting the website.<br><br>
+                    Finally, make those who have the potential to consider and be interested in buying the unit to
+                    fill out the schedule form for the Show Unit private visit available on the website.
+                    The strategy consists of operating social media and advertising campaigns with a variety of
+                    objectives for targeted audiences through Facebook Ads that includes Facebook and Instagram,
+                    as well as Google Ads which includes Search Ads, Display Ads, and Youtube Ads.
+                    After the campaigns launch, LOGIQUE continues to perform a variety of structured approaches
+                    and optimizations to increase traffic every month. It starts from the production and design of
+                    banners in various sizes, making ad copies regularly, evaluating the targetings, A/B testing,
+                    adding keywords, and all efforts to increase the Quality Score so the costs remain low and
+                    effective.<br><br>
+                    Meanwhile, the implementation of SEO on Opus Park website is also conducted gradually. It
+                    starts with adding related keywords, meta tags, setting navigation and elements on the
+                    website, reworking on the contents provided to be SEO friendly, creating Indonesian-language
+                    pages, and many more, including to implement accelerated mobile pages (AMP) on the
+                    website.<br><br>
+                    Here are the Key Performance Indicators (KPI) applied:<br>
+                    1. Reach as many targeted audiences as possible<br>
+                    2. Leading users to the Opus Park website<br>
+                    3. Increasing engagements with users<br>
+                    4. Navigate website visits in conversions<br>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </p>
+            </div>
+          </div>
+        </section>
+        <section class="cs-section">
+          <img src="img/line-4.png" alt="case study logique" class="cs-line cs-line-4">
+          <div class="row">
+            <div class="col-md-12">
+              <p class="text-center" style="font-size: 22px;font-weight: bold;">Results</p>
+              <h2 class="cs-subtitle text-center">Reached Conversions</h2>
+              <p class="cs-desc text-center">
+              Implementing a digital marketing strategy has helped Opus Park increase awareness among a targeted audience. Not only that, the Opus Park website has a significant increase in traffic. The engagement that was obtained was fairly satisfying. This good result has even been seen in 1-2 months since the digital marketing operations were executed in a cost-efficient manner.
+              <br><br>
+              <a class="btn btn-cs" data-toggle="modal" href='#modal-id-2'>Detail</a>
+              <div class="modal fade" id="modal-id-2">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title">Reached Conversions</h4>
+                    </div>
+                    <div class="modal-body">
+                    Implementing a digital marketing strategy has helped Opus Park increase awareness among a targeted audience. Not only that, the Opus Park website has a significant increase in traffic. The engagement that was obtained was fairly satisfying. This good result has even been seen in 1-2 months since the digital marketing operations were executed in a cost-efficient manner. Here are the results achieved from August to November 2018: <br><br>
+                    - Engagements increased by +500,000 in total on Opus Park social media <br>
+                    - +600,000 sessions generated by users on Opus Park website <br>
+                    - The number of bounce rate drops to -9.12%<br>
+                    - +2,000 conversions have been made by users on the website<br>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </p>
+              <img src="img/img-4.png" alt="case study logique" class="img-responsive center-block custom3">
+            </div>
+          </div>
+          <img src="img/orm-2.png" alt="case study logique" class="cs-orm cs-orm-2">
+        </section>
+        <section class="cs-bottom">
+          <h2 class="cs-subtitle">About <img src="/img/logo.png" alt="logo logique" class="cs-logo-bottom"></h2>
+          <p class="cs-desc">
+          As a professional team in website creation services, LOGIQUE provides the best optimized solution for your business. In addition to website creation services, LOGIQUE also provides other services such as digital consulting services, application creation services, web design, web system development, SEO services, and printing. We provide detailed structured designs for the creation of corporate websites, applications, as well as the creation of large-scale websites or systems with a combination of targeted design and good system architecture.
+          </p>
+          <a href="/en/contact.php" class="btn btn-cs-bottom">Contact Us</a>
+          <a href="/en/services.php" class="btn btn-cs-bottom">Our Services</a>
+          <a href="/en/portfolio.php" class="btn btn-cs-bottom">Portfolio</a>
+          <a href="/en/services/mekanisme-pembuatan-website/" class="btn btn-cs-bottom">Web Development</a>
+          <a href="/en/services/pembuatan-sistem-web/" class="btn btn-cs-bottom">Web System Development</a>
+        </section>
+        <footer>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-8">
+                <h2>About Us</h2>
+                <br/>
+                As a professional team in website creation services, LOGIQUE provides the best optimized solution for your business. In addition to website creation services, LOGIQUE also provides other services such as digital consulting services, application creation services, web design, web system development, SEO services, and printing. We provide detailed structured designs for the creation of corporate websites, applications, as well as the creation of large-scale websites or systems with a combination of targeted design and good system architecture.<br/>
+                <div class="visible-sm visible-xs"><br/></div>
+              </div>
+              <div class="col-md-4">
+                <h2>Contact Us</h2>
+                <a href="/en/contact.php" target="_blank"><img alt="Logo" src="../../../img/logo.png" class="logo"/></a> <br />
+                Ad Premier Building 19th Floor. <br/>
+                Jalan Tb. Simatupang No. 5 <br/>
+                Ragunan, Ps. Minggu, <br/>
+                Jakarta Selatan, Indonesia 12550 <br/>
+              </div>
+            </div>
+          </div>
+        </footer>
+        <div class="copyright">
+          © 2018 Logique Digital Indonesia. All Rights Reserved.
+        </div>
+      </div>
+      <?php include '../../../footer.php';?>
+		</div>
+		<script type="application/ld+json">
+			{
+			  "@context": "http://schema.org",
+			  "@type": "Organization",
+			  "address": {
+					"@type": "PostalAddress",
+					"addressLocality": "Jakarta, Indonesia",
+					"postalCode": "12550",
+					"streetAddress": "Ad Premier Building 19th Floor. Jalan Tb. Simatupang No. 5, Ragunan, Ps. Minggu,  Jakarta Selatan"
+			  },
+			  "logo": "https://www.logique.co.id/img/logo.png",
+			  "email": "info(at)logique.co.id",
+			  "telephone": "+62 (21) 2270 8935/36"
+			}
+		</script>
+	</body>
+</html>
