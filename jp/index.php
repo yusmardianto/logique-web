eb1a2e1cfb68b6410debfb72852aacb97e01c51f
Faiq Naufal <nauval@logique.co.id>
2019-03-22 17:10:00 +0700

    adjusted the first slider to same with indonesia page

--- jp/index.php
+++ jp/index.php
@@ -4,18 +4,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="description" itemprop="description" content="インドネシア、ジャカルタでのWEBサイト(ホームページ)制作、システム構築、WEBマーケティング、モバイルアプリ開発(iOS / Andorid)、SNS運用(Facebook/Instagram/Twitter等)、カタログ・パンフレット等のデザインなら日系実績No.1のLOGIQUEデジタルインドネシアにお任せください。">
-	<meta name="keywords" content="WEBサイト制作, ホームページ制作, ウェブシステム構築, インターネットマーケティング, SEO, デザイン, パンフレット, モバイルアプリ">
+    <meta name="description" itemprop="description" content="インドネシア、ジャカルタでのWebサイト(ホームページ)制作、システム構築、Webマーケティング、モバイルアプリ開発(iOS / Andorid)、SNS運用(Facebook/Instagram/Twitter等)、カタログ・パンフレット等のデザインなら日系実績No.1のLOGIQUEデジタルインドネシアにお任せください。">
+	<meta name="keywords" content="Webサイト制作, ホームページ制作, ウェブシステム構築, インターネットマーケティング, SEO, デザイン, パンフレット, モバイルアプリ">
 	<meta name="author" content="PT. Logique Digital Indonesia">
 	<meta itemprop="name" content="PT. Logique Digital Indonesia">
-	<meta property="og:title" content="ジャカルタのWEBサイト/ホームページ制作会社 | LOGIQUE デジタルインドネシア">
+	<meta property="og:title" content="ジャカルタのWebサイト/ホームページ制作会社 | LOGIQUE デジタルインドネシア">
 	<meta property="og:type" content="website">
 	<meta property="og:site_name" content="Logique">
 	<meta property="og:url" content="https://logique.co.id/jp">
 	<meta property="og:image" content="https://www.logique.co.id/img/og-image.png">
-	<meta property="og:description" content="インドネシア、ジャカルタでのWEBサイト(ホームページ)制作、システム構築、Digitalマーケティング、モバイルアプリ開発(ハイブリッド / iOS / Andorid)、SNS運用(Facebook/Instagram/Twitter等)、コンテンツマーケティングなら日系実績No.1のLOGIQUEデジタルインドネシアにお任せください。インドネシアで7年以上多くのお客様をご支援してきた実績があります。">
+	<meta property="og:description" content="インドネシア、ジャカルタでのWebサイト(ホームページ)制作、システム構築、Digitalマーケティング、モバイルアプリ開発(ハイブリッド / iOS / Andorid)、SNS運用(Facebook/Instagram/Twitter等)、コンテンツマーケティングなら日系実績No.1のLOGIQUEデジタルインドネシアにお任せください。インドネシアで7年以上多くのお客様をご支援してきた実績があります。">
 	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
-    <title>ジャカルタのWEBサイト/ホームページ制作会社 | LOGIQUE デジタルインドネシア</title>
+    <title>ジャカルタのWebサイト/ホームページ制作会社 | LOGIQUE デジタルインドネシア</title>
 
     <!-- Bootstrap -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
@@ -40,6 +40,11 @@
      src="https://www.facebook.com/tr?id=713777762291663&ev=PageView&noscript=1"
     /></noscript>
     <!-- End Facebook Pixel Code -->
+    <style type="text/css">
+    	.title-index span {
+    		font-family: initial;
+    	}
+    </style>
     </head>
 	<body>
 		<div class="wrapper o-wrapper top-only">
@@ -129,39 +134,37 @@
 								<div class="item active">
 									<div class="text-center">
 										<h1 class="title-index" >
-											<span class="yellow">インドネシア</span>でお客様の課題を<br><span class="yellow">WEB・IT</span>の活用で解決してきました。
+											<span class="yellow">インドネシア</span>でお客様の課題を<br><span class="yellow">Web・IT</span>の活用で解決してきました。
 										</h1>
-										WEB・ホームページ制作・WEBシステム開発をしています。インドネシア国内のお客様のインターネットビジネスをWEB戦略策定から制作開発、運用までサポートしています。ご予算に応じて成果のでるWEBを制作運用することを心がけています。ネットビジネスコンサルティング、モバイルアプリ開発、SEO対策、ソーシャルメディア運用、コンテンツマーケティング、Video制作、プリントデザイン、サーバーレンタルなど関連サービスも幅広く提供していますのでお気軽にご相談ください。採用管理システムやCRMシステム、ECサイト構築などシステム開発も数多く経験しています。今後もインドネシア、ASEAN諸国や日本のお客様のビジネス発展にインターネット・ITを活用して貢献していきます。<br/><br/>
+										<p style="margin-bottom: 20px;">
+											Web・ホームページ制作・Webシステム開発をしています。インドネシア国内のお客様のインターネットビジネスをWeb戦略策定から制作開発、運用までサポートしています。ご予算に応じて成果のでるWebを制作運用することを心がけています。デジタルマーケティングコンサルティング、モバイルアプリ開発、SEO対策、ソーシャルメディア運用、コンテンツマーケティング、Video制作、プリントデザイン、サーバーレンタルなど関連サービスも幅広く提供していますのでお気軽にご相談ください。採用管理システムやCRMシステム、ECサイト構築などシステム開発も数多く経験しています。今後もインドネシア、ASEAN諸国や日本のお客様のビジネス発展にインターネット・ITを活用して貢献していきます。
+										</p>
 										<div class="row">
 											<div class="col-lg-offset-1 col-lg-10">
 												<div class="row">
 													<div class="col-md-3 col-sm-6">
-														<div class="div-service-cat">
-															<img src="/img/html.png" alt="Jasa Pembuatan Website" class="center-block img-responsive"/>
-															<h2>WEBサイト / ホームページ制作</h2>
-															<a target="_blank" href="/jp/services/mekanisme-pembuatan-website/" class="btn">サービス詳細はこちら</a>
-														</div>
+														<a href="/jp/services/web-dev.php" class="div-service-cat">
+															<img src="/img/top/i-top1.png" alt="Website Creation Service" class="center-block img-responsive"/>
+															<h2>Webサイト / ホームページ制作</h2>
+														</a>
 													</div>
 													<div class="col-md-3 col-sm-6">
-														<div class="div-service-cat">
-															<img src="/img/mobile-solution.png" alt="Jasa Pembuatan Aplikasi" class="center-block img-responsive"/>
+														<a  href="/jp/services/mobile-app-dev.php" class="div-service-cat">
+															<img src="/img/top/i-top2.png" alt="Mobile App Service" class="center-block img-responsive"/>
 															<h2>スマホ / モバイルApp開発</h2>
-															<a target="_blank" href="/jp/services/pembuatan-aplikasi-mobile/" class="btn">サービス詳細はこちら</a>
-														</div>
+														</a>
 													</div>
 													<div class="col-md-3 col-sm-6">
-														<div class="div-service-cat">
-															<img src="/img/jasa-promosi-online.png" alt="Jasa Promosi Online" class="center-block img-responsive"/>
-															<h2 style="padding: 0 30px;">WEBシステム開発</h2>
-															<a target="_blank" href="/jp/services/pembuatan-sistem-web/" class="btn">サービス詳細はこちら</a>
-														</div>
+														<a href="/jp/services/system-dev.php" class="div-service-cat">
+															<img src="/img/top/i-top3.png" alt="Web System Service" class="center-block img-responsive"/>
+															<h2 style="padding: 0 30px;">Webシステム開発</h2>
+														</a>
 													</div>
 													<div class="col-md-3 col-sm-6">
-														<div class="div-service-cat">
-															<img src="/img/jasa-seo.png" alt="Jasa Optimasi SEO" class="center-block img-responsive"/>
-															<div>WEBセキュリティ脆弱性テスト</div>
-															<a target="_blank" href="/jp/services/penetration-testing/" class="btn">サービス詳細はこちら</a>
-														</div>
+														<a href="/jp/services/penetration-test.php" class="div-service-cat">
+															<img src="/img/top/i-top4.png" alt="Security and Penetration Testing" class="center-block img-responsive"/>
+															<h2>Webセキュリティ脆弱性テスト</h2>
+														</a>
 													</div>
 												</div>
 											</div>
@@ -232,11 +235,11 @@
 								</div>
 							  </div>
 							  <a class="left carousel-control" href="#myCarousel2" data-slide="prev">
-								<span class="juggling glyphicon glyphicon-chevron-left"></span>
+								<span class="fa fa-angle-left"></span>
 								<span class="sr-only">Previous</span>
 							  </a>
 							  <a class="right carousel-control" href="#myCarousel2" data-slide="next">
-								<span class="juggling glyphicon glyphicon-chevron-right"></span>
+								<span class="fa fa-angle-right"></span>
 								<span class="sr-only">Next</span>
 							  </a>
 							</div>
@@ -262,39 +265,37 @@
 							<div class="item active">
 								<div class="text-center">
 									<h1 class="title-index" style="font-size:40px">
-										<span class="yellow">インドネシア</span>でお客様の課題を<br><span class="yellow">WEB・IT</span>の活用で解決してきました。
+										<span class="yellow">インドネシア</span>でお客様の課題を<br><span class="yellow">Web・IT</span>の活用で解決してきました。
 									</h1>
-									WEB・ホームページ制作・WEBシステム開発をしています。インドネシア国内のお客様のインターネットビジネスをWEB戦略策定から制作開発、運用までサポートしています。ご予算に応じて成果のでるWEBを制作運用することを心がけています。ネットビジネスコンサルティング、モバイルアプリ開発、SEO対策、ソーシャルメディア運用、コンテンツマーケティング、Video制作、プリントデザイン、サーバーレンタルなど関連サービスも幅広く提供していますのでお気軽にご相談ください。採用管理システムやCRMシステム、ECサイト構築などシステム開発も数多く経験しています。今後もインドネシア、ASEAN諸国や日本のお客様のビジネス発展にインターネット・ITを活用して貢献していきます。<br/><br/>
+									<p style="margin-bottom: 20px;">
+										Web・ホームページ制作・Webシステム開発をしています。インドネシア国内のお客様のインターネットビジネスをWeb戦略策定から制作開発、運用までサポートしています。ご予算に応じて成果のでるWebを制作運用することを心がけています。デジタルマーケティングコンサルティング、モバイルアプリ開発、SEO対策、ソーシャルメディア運用、コンテンツマーケティング、Video制作、プリントデザイン、サーバーレンタルなど関連サービスも幅広く提供していますのでお気軽にご相談ください。採用管理システムやCRMシステム、ECサイト構築などシステム開発も数多く経験しています。今後もインドネシア、ASEAN諸国や日本のお客様のビジネス発展にインターネット・ITを活用して貢献していきます。
+									</p>
 									<div class="row">
 										<div class="col-lg-offset-1 col-lg-10">
 											<div class="row">
 												<div class="col-md-3 col-sm-6">
-													<div class="div-service-cat">
-														<img src="/img/html.png" alt="Jasa Pembuatan Website" class="center-block img-responsive"/>
-														<h2>WEBサイト / ホームページ制作</h2>
-														<a target="_blank" href="/jp/services/mekanisme-pembuatan-website/" class="btn">サービス詳細はこちら</a>
-													</div>
+													<a href="/jp/services/web-dev.php" class="div-service-cat">
+														<img src="/img/top/i-top1.png" alt="Website Creation Service" class="center-block img-responsive"/>
+														<h2>Webサイト / ホームページ制作</h2>
+													</a>
 												</div>
 												<div class="col-md-3 col-sm-6">
-													<div class="div-service-cat">
-														<img src="/img/mobile-solution.png" alt="Jasa Pembuatan Aplikasi" class="center-block img-responsive"/>
+													<a href="/jp/services/mobile-app-dev.php" class="div-service-cat">
+														<img src="/img/top/i-top2.png" alt="Mobile App Service" class="center-block img-responsive"/>
 														<h2>スマホ / モバイルApp開発</h2>
-														<a target="_blank" href="/jp/services/pembuatan-aplikasi-mobile/" class="btn">サービス詳細はこちら</a>
-													</div>
+													</a>
 												</div>
 												<div class="col-md-3 col-sm-6">
-													<div class="div-service-cat">
-														<img src="/img/jasa-promosi-online.png" alt="Jasa Promosi Online" class="center-block img-responsive"/>
-														<h2 style="padding: 0 30px;">WEBシステム開発</h2>
-														<a target="_blank" href="/jp/services/pembuatan-sistem-web/" class="btn">サービス詳細はこちら</a>
-													</div>
+													<a href="/jp/services/system-dev.php" class="div-service-cat">
+														<img src="/img/top/i-top3.png" alt="Web System" class="center-block img-responsive"/>
+														<h2 style="padding: 0 30px;">Webシステム開発</h2>
+													</a>
 												</div>
 												<div class="col-md-3 col-sm-6">
-													<div class="div-service-cat">
-														<img src="/img/jasa-seo.png" alt="Jasa Optimasi SEO" class="center-block img-responsive"/>
-														<div>WEBセキュリティ脆弱性テスト</div>
-														<a target="_blank" href="/jp/services/penetration-testing/" class="btn">サービス詳細はこちら</a>
-													</div>
+													<a class="div-service-cat" href="/jp/services/penetration-test.php">
+														<img src="/img/top/i-top4.png" alt="Security and Penetration Testing Service" class="center-block img-responsive"/>
+														<h2>Webセキュリティ脆弱性テスト</h2>
+													</a>
 												</div>
 											</div>
 										</div>
@@ -303,11 +304,11 @@
 							</div>
 
 							<div class="item">
-								<h1 class="text-center title-index title-index2" style="font-size:38px!important">
+								<h1 class="text-center title-index title-index2">
 									インドネシアで<span class="yellow">HRテック</span>ソリューションを提供しています。
 								</h1>
 								<div class="row">
-									<div class="col-md-6">
+									<div class="col-md-10 col-md-offset-1 text-center">
 										<div class="row">
 											<div class="col-sm-6">
 												<div class="onhover">
@@ -373,11 +374,11 @@
 							</div>
 						  </div>
 						  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-							<span class="juggling glyphicon glyphicon-chevron-left"></span>
+							<span class="fa fa-angle-left"></span>
 							<span class="sr-only">Previous</span>
 						  </a>
 						  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-							<span class="juggling glyphicon glyphicon-chevron-right"></span>
+							<span class="fa fa-angle-right"></span>
 							<span class="sr-only">Next</span>
 						  </a>
 						</div>
