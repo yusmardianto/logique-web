ec46407b195c82c75ad0a54e7acaa27a46b20988
Faiq Naufal <nauval@logique.co.id>
2019-04-22 16:57:04 +0700

    added global navi and new footer

--- en/product/sugoi-saiyo.php
+++ en/product/sugoi-saiyo.php
@@ -71,6 +71,68 @@ staffing solutions, recruitment tools, sugoi saiyo, logique digital Indonesia">
 	</script>
 </head>
 	<body>
+		<div class="nav-lp">
+			<button type="button" class="navbar-toggle collapsed" aria-label="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar first"></span>
+				<span class="icon-bar second"></span>
+				<span class="icon-bar third"></span> 
+			</button>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<div class="backdrop-test"></div>
+				<div class="logique-nav">
+					<div class="margin-for-nav">
+						<div class="text-center">
+							<a href="/en/index.php">
+								<div class="img-home"></div>
+								Home
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/about.php">
+								<div class="img-about"></div>
+								About Us
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/portfolio.php">
+								<div class="img-portfolio"></div>
+								Portfolio
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/services.php">
+								<div class="img-services"></div>
+								Services
+							</a>
+						</div>
+					 	<div class="text-center active">
+							<a href="/en/product.php">
+								<div class="img-product"></div>
+								Product
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/joinus.php">
+								<div class="img-career"></div>
+								Career
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/en/contact.php">
+								<div class="img-contact"></div>
+								Contact Us
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="https://www.logique.co.id/blog/" target="_blank" rel="noreferrer">
+								<div class="img-blog"></div>
+								Blog
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<header>
 			<div class="container">
 				<div class="row">
@@ -448,34 +510,9 @@ staffing solutions, recruitment tools, sugoi saiyo, logique digital Indonesia">
 				</div>
 			</div>
 		</section>
-		<footer>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
-						<img src="img/sugoi-saiyo/logo.png" alt="Sugoi Saiyo Logo" class="img-responsive">
-					</div>
-					<div class="col-md-4">
-						<p class="address">
-							Ad Premier Building 19th Floor Jalan Tb. Simatupang No. 5 Ragunan, Ps. Minggu, Jakarta Selatan, Indonesia 12550
-						</p>
-						<p class="link">
-							<span>Product LOGIQUE :</span>
-							<a href="/../produk/nippo.php">Nippo</a>
-							<a href="/../produk/dokodemo-kerja.php">Dokodemo-Kerja</a>
-						</p>
-					</div>
-					<div class="col-md-4">
-						<p class="phone">+62(21) 2270 8935 / 36</p>
-						<p class="email"><a href="mailto:info@sugoi-saiyo.co.id">info@logique.co.id</a></p>
-					</div>
-				</div>
-			</div>
-		</footer>
-
-		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script src="/js/bootstrap.min.js"></script>
+		<?php include '../footer.php'; ?>
+		
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.2/jquery.matchHeight-min.js"></script>
-		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<script type="text/javascript">
 			$(function() {
 				$('.smooth').click(function() {
--- produk/sugoi-saiyo.php
+++ produk/sugoi-saiyo.php
@@ -78,6 +78,68 @@ if(isset($_POST['submitted']))
 	<!-- End Twitter universal website tag code -->
 </head>
 	<body>
+		<div class="nav-lp">
+			<button type="button" class="navbar-toggle collapsed" aria-label="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar first"></span>
+				<span class="icon-bar second"></span>
+				<span class="icon-bar third"></span> 
+			</button>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<div class="backdrop-test"></div>
+				<div class="logique-nav">
+					<div class="margin-for-nav">
+						<div class="text-center">
+							<a href="/">
+								<div class="img-home"></div>
+								Beranda
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/tentang-kami.php">
+								<div class="img-about"></div>
+								Tentang Kami
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/portofolio.php">
+								<div class="img-portfolio"></div>
+								Portfolio
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/layanan.php">
+								<div class="img-services"></div>
+								Layanan
+							</a>
+						</div>
+					 	<div class="text-center active">
+							<a href="/produk.php">
+								<div class="img-product"></div>
+								Produk
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/karir.php">
+								<div class="img-career"></div>
+								Karir
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="/hubungi-kami.php">
+								<div class="img-contact"></div>
+								Hubungi Kami
+							</a>
+						</div>
+						<div class="text-center">
+							<a href="https://www.logique.co.id/blog/" target="_blank" rel="noreferrer">
+								<div class="img-blog"></div>
+								Blog
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<header>
 			<div class="container">
 				<div class="row">
@@ -455,34 +517,9 @@ if(isset($_POST['submitted']))
 				</div>
 			</div>
 		</section>
-		<footer>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
-						<img src="img/sugoi-saiyo/logo.png" alt="Sugoi Saiyo Logo" class="img-responsive">
-					</div>
-					<div class="col-md-4">
-						<p class="address">
-							Ad Premier Building 19th Floor Jalan Tb. Simatupang No. 5 Ragunan, Ps. Minggu, Jakarta Selatan, Indonesia 12550
-						</p>
-						<p class="link">
-							<span>Product LOGIQUE :</span>
-							<a href="/../produk/nippo.php">Nippo</a>
-							<a href="/../produk/dokodemo-kerja.php">Dokodemo-Kerja</a>
-						</p>
-					</div>
-					<div class="col-md-4">
-						<p class="phone">+62(21) 2270 8935 / 36</p>
-						<p class="email"><a href="mailto:info@sugoi-saiyo.co.id">info@logique.co.id</a></p>
-					</div>
-				</div>
-			</div>
-		</footer>
-		
-		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script src="/js/bootstrap.min.js"></script>
+		<?php include '../footer.php'; ?>
+	
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.2/jquery.matchHeight-min.js"></script>
-		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<script type="text/javascript">			
 			$(function() {
 				$('.smooth').click(function() {
