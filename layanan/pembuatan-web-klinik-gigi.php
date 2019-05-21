b695264c4cf7e6224df4bef54f7fdaef717385c7
Carissa Limantara <carissa@logique.co.id>
2019-05-21 10:00:01 +0700

    col adjustment

--- layanan/pembuatan-web-klinik-gigi.php
+++ layanan/pembuatan-web-klinik-gigi.php
@@ -173,7 +173,7 @@ if(isset($_POST['submitted']))
 				<div class="container">
 					<h1 class="s-title">Apa pentingnya website pribadi bagi klinik gigi?</h1>
 					<div class="row">
-						<div class="col-md-4 col-md-offset-1 col-sm-8">
+						<div class="col-md-5 col-md-offset-1 col-sm-8">
 							<p>
 							Dengan memiliki website pribadi, Anda dapat memperkenalkan produk dan jasa klinik gigi Anda dengan tampilan yang unik dan berkarakter. Berbeda dengan menggunakan portal kesehatan atau media sosial, Anda dapat memilliki tampilan website yang berbeda dengan yang lain sehingga membuat para pengunjung yang juga pasien potensial tertarik mengunjungi klinik gigi Anda. 
 							Anda juga dapat meningkatkan jumlah pengunjung website melalui Search Engine Optimalization (SEO) jika menggunakan website sendiri. Selain itu, website Anda dapat memberikan informasi kepada pengunjung setiap saat tentang produk atau jasa yang klinik Anda tawarkan. Melalui website pribadi, Anda juga menciptakan brand yang mendorong kredibilitas klinik Anda melesat naik.
@@ -221,7 +221,7 @@ if(isset($_POST['submitted']))
 				<div class="container">
 					<h2 class="s-title">Masalah yang Sering Dihadapi dalam Memiliki Website</h2>
 					<div class="row">
-						<div class="col-md-8 col-md-offset-2">
+						<div class="col-md-10 col-md-offset-1">
 							<div class="row">
 								<div class="col-md-4 col-sm-4 clearfix">
 									<img src="assets/img/img-dentist/new/i-problem1.png" alt="masalah klinik gigi">
@@ -243,9 +243,12 @@ if(isset($_POST['submitted']))
 
 			<section class="solusi">
 				<div class="container">
-					<img src="assets/img/img-dentist/new/solution.png" alt="solusi klinik gigi" class="img-responsive solusi-title">
 					<div class="row">
-						<div class="col-md-8 col-md-offset-2">
+						<img src="assets/img/img-dentist/new/solution.png" alt="solusi klinik gigi" class="img-responsive solusi-title">
+					</div>
+
+					<div class="row">
+						<div class="col-md-10 col-md-offset-1">
 							<div class="row">
 								<div class="col-md-4 col-sm-4 clearfix">
 									<img src="assets/img/img-dentist/new/i-solution1.png" alt="solusi klinik gigi">
@@ -268,7 +271,7 @@ if(isset($_POST['submitted']))
 			<section class="layanan">
 				<div class="container">
 					<div class="row">
-						<div class="col-md-8 col-md-offset-2">
+						<div class="col-md-10 col-md-offset-1">
 							<div class="row">
 								<div class="col-md-4 col-sm-4">
 									<div class="d-layanan l1">
@@ -428,7 +431,7 @@ if(isset($_POST['submitted']))
 			<section class="biaya">
 				<div class="container">
 					<div class="row">
-						<div class="col-md-6 col-md-offset-3">
+						<div class="col-md-8 col-md-offset-2"><br>
 							<h2 class="s-title">Semua masalah diatas dapat diselesaikan melalui website berkualitas hanya dengan:</h2>
 							<div class="price-badge">
 								<img src="assets/img/img-dentist/new/i-tag.png" alt="harga website klinik gigi"> RP. 500,000<sub>/bulan</sub>
@@ -436,7 +439,7 @@ if(isset($_POST['submitted']))
 							<div class="row">
 								<div class="col-md-6 col-sm-6">
 									<h4>Biaya Awal :</h4>
-									<p><b>Rp3.000.000</b> dengan masa kontrak <b>minimal 6 bulan.</b> Sudah termasuk Web Server, Domain, dan SSL.</p>
+									<p><b>Rp3.000.000**</b> dengan masa kontrak <b>minimal 6 bulan.</b> Sudah termasuk Web Server, Domain, dan SSL.</p>
 								</div>
 								<div class="col-md-6 col-sm-6">
 									<h4>Selama Masa Kontrak :</h4>
@@ -445,7 +448,8 @@ if(isset($_POST['submitted']))
 							</div>
 							<div class="row">
 								<div class="col-md-10 col-md-offset-1">
-									<p>** Biaya awal dengan “Blog” adalah Rp5.000.000 <br>Biaya bulanan: Rp9.000.000</p>
+									<p>** Biaya awal dengan “Blog” adalah Rp5.000.000 <br>Biaya bulanan: Rp900.000</p>
+									<p>Dapatkan potongan harga untuk kontrak jangka panjang</p>
 								</div>
 							</div>
 						</div>
