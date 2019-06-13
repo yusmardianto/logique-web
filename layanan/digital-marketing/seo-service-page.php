6d0a732270e70cf1ce287797f8ed721668ea9d90
Faiq Naufal <nauval@logique.co.id>
2019-06-13 20:18:24 +0700

    added seo maintenance page

---
+++ layanan/digital-marketing/seo-service-page.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="PT LOGIQUE DIGITAL INDONESIA ">
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+	<title>Digital Marketing | LOGIQUE Digital Indonesia</title>
+	<link href="/css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style-seo-service-page.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+	<link href="/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<script>
+		(function(i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function() {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+		ga('create', 'UA-105878648-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+	<!-- Twitter universal website tag code <-->
+	</-->
+	<script>
+		! function(e, t, n, s, u, a) {
+			e.twq || (s = e.twq = function() {
+					s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+				}, s.version = '1.1', s.queue = [], u = t.createElement(n), u.async = !0, u.src = '//static.ads-twitter.com/uwt.js',
+				a = t.getElementsByTagName(n)[0], a.parentNode.insertBefore(u, a))
+		}(window, document, 'script');
+		// Insert Twitter Pixel ID and Standard Event data below
+		twq('init', 'o0xru');
+		twq('track', 'PageView');
+	</script>
+	<!-- End Twitter universal website tag code -->
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="nav-lp">
+  		<button type="button" class="navbar-toggle collapsed" aria-label="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+  			<span class="icon-bar first"></span>
+  			<span class="icon-bar second"></span>
+  			<span class="icon-bar third"></span>
+  		</button>
+  		<div class="collapse navbar-collapse" id="myNavbar">
+  			<div class="backdrop-test"></div>
+  			<div class="logique-nav">
+  				<div class="margin-for-nav">
+  					<div class="text-center">
+  						<a href="/">
+  							<div class="img-home"></div>
+  							Beranda
+  						</a>
+  					</div>
+  					<div class="text-center">
+  						<a href="/logique.php">
+  							<div class="img-about"></div>
+  							Tentang Kami
+  						</a>
+  					</div>
+  					<div class="text-center">
+  						<a href="/portofolio.php">
+  							<div class="img-portfolio"></div>
+  							Portfolio
+  						</a>
+  					</div>
+  					<div class="text-center active">
+  						<a href="/layanan.php">
+  							<div class="img-services"></div>
+  							Layanan
+  						</a>
+  					</div>
+  					<div class="text-center">
+  						<a href="/produk.php">
+  							<div class="img-product"></div>
+  							Produk
+  						</a>
+  					</div>
+  					<div class="text-center">
+  						<a href="/karir.php">
+  							<div class="img-career"></div>
+  							Karir
+  						</a>
+  					</div>
+  					<div class="text-center">
+  						<a href="/hubungi-kami.php">
+  							<div class="img-contact"></div>
+  							Hubungi Kami
+  						</a>
+  					</div>
+  					<div class="text-center">
+  						<a href="https://www.logique.co.id/blog/" target="_blank" rel="noreferrer">
+  							<div class="img-blog"></div>
+  							Blog
+  						</a>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+		</div>
+    <div class="content-area">
+      <div class="language-btn">
+				<ul class="list-inline">
+					<li><a href="/en/services/digital-marketing.php">EN</a></li>
+					<li class="active"><a href="#">ID</a></li>
+					<li><a href="/jp/services/digital-marketing.php">JP</a></li>
+				</ul>
+			</div>
+      <div class="div-top">
+				<h1 class="title">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<img src="../assets/img/img-digital-marketing/logo.png" alt="Logo" />
+								LOGIQUE Digital Marketing
+							</div>
+						</div>
+					</div>
+				</h1>
+				<div class="breadcrumb-section">
+					<ol class="breadcrumb">
+						<li><a href="#">Beranda</a></li>
+						<li><a href="#">Layanan</a></li>
+						<li><a href="#">Digital Marketing</a></li>
+						<li class="active"><a href="#">Layanan SEO</a></li>
+					</ol>
+				</div>
+        <!-- Banner -->
+        <div class="banner-seo-service">
+          <div class="row">
+            <div class="col-md-6 col-lg-5 col-lg-offset-1">
+              <img class="img-responsive" src="img/img-seo-maintenance/seo-maintenance-banner1.png" alt="Seo Maintenance">
+            </div>
+            <div class="col-md-5 col-lg-5">
+              <h2><strong>Seo Maintenance</strong></h2>
+              <p>
+                <strong style="font-size: 26px;">LOGIQUE</strong>
+                <br>
+                memberikan layanan SEO maintenance yang dilakukan secara berkala untuk meningkatkan jumlah traffic pengunjung dan menjaga agar website Anda tetap berada di posisi teratas dari mesin pencari. Dengan harga mulai dari <strong style="font-size: 20px;">Rp5.000.000/bulan</strong> Anda sudah bisa mendapatkan banyak layanan SEO dari kami, seperti: search rank report, analytics report, konsultasi SEO, search console and web maintenance (termasuk SEO), serta penulisan/ penyesuaian konten.
+              </p>
+            </div>
+          </div>
+          <div class="list-seo-services">
+						<div class="row">
+							<div class="col-lg-offset-2 col-lg-8">
+								<div class="row">
+									<div class="col-sm-4">
+										<div class="seo-services">
+											<img class="img-responsive" src="img/img-seo-maintenance/search-robot.png" alt="Search Rank Report">
+											<span>SEARCH RANK REPORT</span>
+										</div>
+									</div>
+									<div class="col-sm-4">
+										<div class="seo-services">
+											<img class="img-responsive" src="img/img-seo-maintenance/analytics-report.png" alt="Analytics Report">
+											<span>ANALYTICS REPORT</span>
+										</div>
+									</div>
+									<div class="col-sm-4">
+										<div class="seo-services">
+											<img class="img-responsive" src="img/img-seo-maintenance/speaker.png" alt="Konsultasi SEO">
+											<span>KONSULTASI SEO</span>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-sm-4">
+										<div class="seo-services">
+											<img class="img-responsive" src="img/img-seo-maintenance/search-service.png" alt="Search Console Services">
+											<span>SEARCH CONSOLE SERVICES</span>
+										</div>
+									</div>
+									<div class="col-sm-4">
+										<div class="seo-services">
+											<img class="img-responsive" src="img/img-seo-maintenance/web-maintenance.png" alt="WEB MAINTENANCE INCLUDE SEO">
+											<span>PEMELIHARAAN WEB (WEB MAINTENANCE) TERMASUK SEO</span>
+										</div>
+									</div>
+									<div class="col-sm-4">
+										<div class="seo-services">
+											<img class="img-responsive" src="img/img-seo-maintenance/content-writing.png" alt="Penulisan / Penyesuaian Konten">
+											<span>PENULISAN / PENYESUAIAN KONTEN</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+          </div>
+        </div>
+				<div class="black-separation"></div>
+				<div class="banner-black-white-hat">
+					<div class="row equal-height">
+						<div class="col-sm-offset-1 col-sm-5 col-lg-offset-2 col-lg-4">
+							<div class="center-v-h">
+								<h3><strong>LOGIQUE</strong> Provides Service with White Hat SEO</h3>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-5 col-lg-4">
+							<img class="img-responsive" src="img/img-seo-maintenance/laptop-white-hat.png" alt="White Hat SEO">
+						</div>
+					</div>
+				</div>
+				<div class="crowd-banner-container">
+					<div class="row">
+						<div class="col-sm-offset-1 col-sm-10 col-lg-offset-2 col-lg-8">
+							<div class="crowd-banner">
+								<p class="text-center">LOGIQUE is the best SEO service company in Jakarta those provides SEO services with white Hat SEO implementation strategies. Logique is proven to help various national and international clients to reach their business goals.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="">
+						<div class="">
+							<img class="img-responsive" src="img/img-seo-maintenance/white-hat.png" alt="White Hat SEO">
+						</div>
+						<p>What Is White Hat SEO? White Hat SEO is one of SEO strategies that follows search engines regulations. The goal in implementing the White Hat SEO technique is to acquire first position on the search engine. Companies or agencies that provide professional SEO services will use White Hat SEO strategies.</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="">
+						<div class="">
+							<img class="img-responsive" src="img/img-seo-maintenance/black-hat.png" alt="Black Hat SEO">
+						</div>
+						<p>The opposite one is Black Hat SEO. Black Hat SEO is an SEO strategy that is opposite with SEO White Hat. This SEO strategy does not follow the search engines regulations.</p>
+					</div>
+				</div>
+			</div>
+			<div class="reason-black-hat-bad">
+				<div class="row equal-height">
+					<div class="col-xs-12 col-sm-12 col-md-offset-1 col-md-5 col-lg-offset-0 col-lg-4">
+						<div class="center-v-h">
+							<img class="img-responsive" src="img/img-seo-maintenance/laptop-rocket.png" alt="Reason Black Hat Bad">
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-8">
+						<p>Therefore, SEO practitioners who implement this strategy will not create an informative content to the users. SEO optimization is only intended to make the page appear on the first page of search engines. After the user searched and read the contents, the information in the content is less than SEO White Hat content. Companies or agencies that provide professional SEO services will not implementing a Black Hat SEO strategy to minimize the risks.</p>
+					</div>
+				</div>
+			</div>
+			<div class="seo-hat-characteristics">
+				<div class="row equal-height">
+					<div class="col-sm-offset-1 col-sm-5">
+						<div class="card-info">
+							<h4 class="text-center">White Hat SEO</h4>
+							<ul>
+								<li>Analyze keywords that are relevant to the content</li>
+								<li>Create link building that relevant with the content</li>
+								<li>Write articles and informative content for users</li>
+								<li>Create user-friendly navigation website page</li>
+								<li>Input relevant keywords in meta tags of a site's page</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-5">
+						<div class="card-info">
+							<h4 class="text-center">Black Hat SEO</h4>
+							<ul>
+								<li>Keyword stuffing (Input a large number of keywords)</li>
+								<li>Enter irrelevant keywords</li>
+								<li>Hidden text (hiding text to trick search engine crawlers)</li>
+								<li>hange the site page after page has been ranked in a search engine.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="is-it-important">
+				<h3><strong>Is It Important to Choose Companies that Implement White Hat SEO?</strong></h3>
+				<p>
+					Yes, it is. The implementation of Black Hat SEO can make your site pages being block by Google and other search engines.
+				</p>
+				<p>
+					As the most popular search engine, Google has been visited by billions of users every day. Your site has great potential to get traffic and new users from those large number of visits.
+				</p>
+				<p>
+					Google is an important source to your website traffic. Blocked by Google can dramatically reduce your business traffic. In addition, Google does not guarantee that your site will acquire a rank in search engine again after your site has been blocked.
+				</p>
+				<p>
+					It can be a serious risk to your site and business if your site has been blocked by Google. So why do you have to take enormous risks?
+				</p>
+				<span>LOGIQUE is ready to help implement White Hat SEO for your business!</span>
+			</div>
+      <?php include '../../footer.php'; ?>
+    </div>
+  </div>
+</body>
+
+</html>
