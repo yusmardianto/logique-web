9fef5f3cc9560156fd258eb21b9290bfcb4a94cc
unknown <calvin@logique.co.id>
2019-06-13 10:21:55 +0700

    added seo-optimization page (banner, explanation, cards)

---
+++ layanan/digital-marketing/seo-optimization.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0">
+	<meta name="description" content="Jika Anda tertarik menerapkan strategi digital marketing untuk bisnis Anda, LOGIQUE dapat membantu meningkatkan profit bisnis Anda lewat SEO, kampanye iklan Google Ads, Facebook Ads, dan lain-lain.">
+	<meta name="keywords" content="digital marketing, pemasaran digital, strategi digital marketing, layanan digital marketing, digital marketing logique">
+	<meta name="author" content="PT LOGIQUE DIGITAL INDONESIA ">
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+	<title>Digital Marketing | LOGIQUE Digital Indonesia</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style-seo-optimization.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+	<link href="/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<script>
+		(function(i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function() {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+		ga('create', 'UA-105878648-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+	<!-- Twitter universal website tag code <-->
+	</-->
+	<script>
+		! function(e, t, n, s, u, a) {
+			e.twq || (s = e.twq = function() {
+					s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+				}, s.version = '1.1', s.queue = [], u = t.createElement(n), u.async = !0, u.src = '//static.ads-twitter.com/uwt.js',
+				a = t.getElementsByTagName(n)[0], a.parentNode.insertBefore(u, a))
+		}(window, document, 'script');
+		// Insert Twitter Pixel ID and Standard Event data below
+		twq('init', 'o0xru');
+		twq('track', 'PageView');
+	</script>
+	<!-- End Twitter universal website tag code -->
+</head>
+
+<body>
+	<div class="wrapper">
+		<div class="nav-lp">
+		<button type="button" class="navbar-toggle collapsed" aria-label="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar first"></span>
+			<span class="icon-bar second"></span>
+			<span class="icon-bar third"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<div class="backdrop-test"></div>
+			<div class="logique-nav">
+				<div class="margin-for-nav">
+					<div class="text-center">
+						<a href="/">
+							<div class="img-home"></div>
+							Beranda
+						</a>
+					</div>
+					<div class="text-center">
+						<a href="/logique.php">
+							<div class="img-about"></div>
+							Tentang Kami
+						</a>
+					</div>
+					<div class="text-center">
+						<a href="/portofolio.php">
+							<div class="img-portfolio"></div>
+							Portfolio
+						</a>
+					</div>
+					<div class="text-center active">
+						<a href="/layanan.php">
+							<div class="img-services"></div>
+							Layanan
+						</a>
+					</div>
+					<div class="text-center">
+						<a href="/produk.php">
+							<div class="img-product"></div>
+							Produk
+						</a>
+					</div>
+					<div class="text-center">
+						<a href="/karir.php">
+							<div class="img-career"></div>
+							Karir
+						</a>
+					</div>
+					<div class="text-center">
+						<a href="/hubungi-kami.php">
+							<div class="img-contact"></div>
+							Hubungi Kami
+						</a>
+					</div>
+					<div class="text-center">
+						<a href="https://www.logique.co.id/blog/" target="_blank" rel="noreferrer">
+							<div class="img-blog"></div>
+							Blog
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+		<div class="content-area">
+			<div class="language-btn">
+				<ul class="list-inline">
+					<li><a href="/en/services/digital-marketing.php">EN</a></li>
+					<li class="active"><a href="#">ID</a></li>
+					<li><a href="/jp/services/digital-marketing.php">JP</a></li>
+				</ul>
+			</div>
+			<div class="div-top">
+				<h1 class="title">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<img src="../assets/img/img-digital-marketing/logo.png" alt="Logo" />
+								LOGIQUE Digital Marketing
+							</div>
+						</div>
+					</div>
+				</h1>
+				<div class="breadcrumb-section">
+					<ol class="breadcrumb">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Service</a></li>
+						<li><a href="#">Digital Marketing</a></li>
+						<li class="active"><a href="#">SEO Optimization</a></li>
+					</ol>
+				</div>
+				<!-- <br/><br/><div class="hidden-xs"><br/></div> -->
+
+				<div class="banner">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-10">
+							<div class="banner-text-wrapper">
+								<h1><strong>LOGIQUE,</strong></h1>
+								<p> Web Developer Profesional Untuk Maksimalisasi SEO secara Menyeluruh</p>
+								
+							</div>
+							<img class="img-fly-paper" src="img/img-seo-optimization/fly-paper.png" />
+						</div>
+						<div class="col-lg-6 col-md-6">
+							<div class="img-web-illustration">
+								<img src="img/img-seo-optimization/web-illustration.png" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="explanation">
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-12">
+						<img class="img-working-guy" src="img/img-seo-optimization/working-guy.png"/>
+					</div>
+					<div class="col-lg-5 col-md-5 col-sm-8 col-xs-8 description">
+						<p>Sebagai perusahaan web developer, <strong>LOGIQUE</strong> tidak hanya mampu membuat dan mengembangkan website, namun juga <strong>mengoptimalisasi penerapan SEO secara teknis pada website tersebut.</strong> Mulai dari pemasangan schema markup, optimasi URL, meta tag, pengaturan redirect, hingga optimasi tampilan dan struktur versi mobile.</p>
+						<br>
+						<p>Hal-hal ini sangat penting, mengingat algoritma mesin pencarian saat ini semakin rumit dengan juga melibatkan UI/UX yang baik. Semakin banyak interaksi positif yang dilakukan visitor pada website Anda, mesin pencarian Google akan semakin meningkatkan peringkat website Anda pada hasil pencarian dari keyword yang Anda targetkan. </p>
+					</div>
+				</div>
+			</div>
+			<div class="cards">
+				<div class="row">
+					<div class="col-lg-4 col-md-5 col-sm-8 col-xs-10 col-lg-offset-2 col-md-offset-1 col-sm-offset-2 col-xs-offset-1">
+						<div class="card">
+							<div class="card-ribbon">
+								<h3>Meningkatkan SEO Dari Perspektif Web Development</h3>
+								<img class="img-responsive" src="img/img-seo-optimization/yellow-ribbon.png" />
+							</div>
+							<div class="card-content">
+								<img class="center-block img-responsive" src="img/img-seo-optimization/web-structure.png"/>
+								<p>SEO bukan hanya tentang optimasi keyword, maksimalisasi query berkaitan yang dicari user, atau konten yang menarik. Ada banyak aspek teknis yang bekerja di belakangnya, yang mendukung agar sebuah web page dapat menduduki peringkat satu di halaman pertama hasil pencarian. Sebuah website dapat meraih posisi tersebut tidak hanya dengan konten yang baik, namun juga bagaimana website tersebut bisa seresponsif mungkin jika sudah diakses visitor. Sebab, ini akan berdampak langsung pada pengalaman menjelajah visitor pada website Anda (user experience).</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-5 col-sm-8 col-xs-10 col-md-offset-0 col-sm-offset-2 col-xs-offset-1">
+					<div class="card">
+							<div class="card-ribbon">
+								<h3>LOGIQUE, Web Developer Profesional di Bidang SEO</h3>
+								<img class="img-responsive" src="img/img-seo-optimization/yellow-ribbon.png" />
+							</div>
+							<div class="card-content">
+								<img class="center-block img-responsive" src="img/img-seo-optimization/increasing.png"/>
+								<p>Dengan para ahli di bidangnya, kami mampu menjadikan website Anda responsif, dinamis, loading cepat, user-friendly, dan memiliki tampilan yang fleksibel serta konsisten di segala perangkat, terutama pada mobile dengan pembuatan AMP. Pengaturan desain juga penting untuk menarik sebanyak mungkin visitor yang cocok dengan bisnis Anda. Web Designer kami akan memasang fitur desain, widget, style element, gambar, dan teks yang menarik secara khusus untuk target Anda. Fitur-fitur seperti ini seringkali tidak dapat Anda temukan di platform situs pengembang website praktis seperti Wordpress atau Joomla.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="thats-why">
+				<div class="no-1">
+					<img class="center-block img-responsive" src="img/img-seo-optimization/no-1.png"/>
+					<p>First page on search engine results</p>
+				</div>
+				<div class="branch">
+					<img class="center-block img-responsive" src="img/img-seo-optimization/arrow-branch.png"/>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+					<img class="center-block img-responsive" src="img/img-seo-optimization/web-dev.png"/>
+					</div>
+					<div class="col-sm-4">
+					<img class="center-block img-responsive" src="img/img-seo-optimization/arrow-branch.png"/>
+					</div>
+					<div class="col-sm-4">
+					<img class="center-block img-responsive" src="img/img-seo-optimization/arrow-branch.png"/>
+					</div>	
+				</div>
+			</div>
+
+			<?php include '../../footer.php'; ?>
+		</div>
+
+	</div>
+
+
+
+
+</body>
+
+</html>
