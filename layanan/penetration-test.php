4e2b7d6e29cafdc99a81ae1caa24ef9af72c3a4b
Bagas Ranggen <bagasranggen@gmail.com>
2019-10-09 16:36:02 +0700

    revise content pen-test LP

--- layanan/penetration-test.php
+++ layanan/penetration-test.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0">
     <meta name="description"
-        content="LOGIQUE dapat membantu meningkatkan sistem keamanan website dan aplikasi menggunakan metode penetration testing. Segera hubungi LOGIQUE untuk mendapatkan layanan security assessment terbaik di Indonesia.">
+        content="LOGIQUE dapat membantu mengevaluasi sistem keamanan website dan aplikasi menggunakan metode penetration testing. Segera hubungi LOGIQUE untuk mendapatkan layanan security assessment terbaik di Indonesia.">
     <meta name="keywords"
         content="penetration test, penetration testing, jasa penetration test, jasa penetration testing, logique, pen test Jakarta, penetration test Indonesia, security assessment Indonesia">
     <meta name="author" content="Logique Digital Indonesia">
@@ -120,18 +120,6 @@
                     mempedulikan keamanan website. Bahkan, penelitian menunjukkan bahwa sekitar 77% perusahaan belum
                     memiliki rencana untuk menerapkan cyber security untuk melindungi data mereka. Hal ini tentu membuat
                     perusahaan rentan untuk diretas dan kehilangan data sensitif tanpa mereka ketahui.</p>
-                <!-- <p>
-                    Masalah <b>keamanan cyber</b> selalu menjadi upaya yang terus dilakukan setiap harinya, terutama
-                    <b>dalam hal bisnis</b>. Jika Anda tidak pernah peduli terhadap keamanan data Anda dan bagaimana
-                    cara melindunginya, maka saat ini adalah waktu yang tepat untuk memulainya.<br /><br />
-                    Tren mengenai masalah keamanan cyber semakin lama <b>semakin meningkat</b> setiap harinya, bahkan
-                    masalah terbesar yang terjadi selama ini adalah banyaknya data yang diretas dan dilanggar dari
-                    berbagai sumber, terutama saat berada di tempat kerja, seperti peretasan pada perangkat seluler dan
-                    perangkat-perangkat yang terhubung ke Internet lainnya.<br /><br />
-                    Bahkan, <b>penelitian terbaru</b> menunjukkan jika sebagian besar perusahaan mempunyai <b>data yang
-                        tidak dilindungi</b> dengan baik dari praktik-praktik cybersecurity. Hal ini tentu membuat
-                    mereka rentan untuk diretas dan <b>kehilangan data tanpa mereka ketahui</b>.
-                </p> -->
             </div>
         </div>
     </div>
@@ -172,19 +160,6 @@
                                     sebagian besar korbannya adalah perusahaan. Sepanjang tahun 2018, telah terjadi
                                     sekitar 4000 laporan terkait kasus kejahatan cyber, dan dari jumlah tersebut kasus
                                     serangan cyber paling banyak terjadi di wilayah Jakarta.<br /><br />
-
-
-                                    <!-- Pernahkah Anda mengetahui, jika setiap hari terdapat 24.000 jenis aplikasi seluler
-                                    yang diblokir karena berpotensi terjadi pelanggaran data di dalamnya. Selain itu,
-                                    serangan terhadap seluruh perangkat yang terhubung ke Internet juga terus mengalami
-                                    peningkatan. <br /><br />
-                                    Bahkan di tahun 2017, angka ini naik sebanyak 600 persen. Bukan hanya itu saja,
-                                    setiap tahunnya serangan ransomware juga terus mengalami pertumbuhan hingga lebih
-                                    dari 350 persen setiap tahunnya.<br /><br />
-                                    Berdasarkan data statistik tersebut dan dari berbagai kasus yang terjadi di seluruh
-                                    dunia, peningkatan pelanggaran data terus terjadi dalam skala yang besar. Namun,
-                                    bukannya membaik, pelanggaran data yang terjadi justru semakin lama semakin
-									bertambah parah setiap harinya. -->
 
                                 </div>
                                 <div class="row">
@@ -448,24 +423,6 @@
                                     konteks hukum merupakan kegiatan yang legal karena sebelumnya telah terjadi kontrak
                                     antara kedua belah pihak. <br /><br />
                                 </p>
-
-
-                                <!-- <p><br />
-                                    <b>Penetration testing</b> atau pentest merupakan istilah yang sering digunakan saat
-                                    seseorang melakukan kegiatan pengujian sebuah sistem atau aplikasi. Kegiatan
-                                    pengujian ini dilakukan untuk mengetahui apakah sistem keamanan yang terdapat pada
-                                    sistem atau aplikasi mempunyai celah sehingga dapat segera diperbaiki dengan
-                                    melakukan patch atau penambalan sehingga sistem keamanan yang terdapat pada suatu
-                                    sistem atau aplikasi tersebut menjadi semakin kuat. Selain melakukan pengujian,
-                                    kegiatan pentest juga melakukan dokumentasi sistem keamanan dari sistem atau
-                                    aplikasi yang akan dilakukan pentest untuk selanjutnya dibuatkan laporan atau report
-                                    kepada perusahaan/ pelanggan.<br /><br />
-
-                                    Sebelum melakukan pentest, biasanya akan terjadi kontrak antara auditor/ pentester
-                                    dengan perusahaan yang ingin aplikasi atau sistemnya dilakukan pentest. Hal ini
-                                    dalam konteks hukum merupakan kegiatan legal karena sebelumnya telah terjadi kontrak
-                                    antara keduanya.
-                                </p> -->
                             </div>
                         </div>
                         <br /><br />
@@ -483,12 +440,14 @@
                             class="yellow">penetrasi testing</span>, yaitu meliputi:</div>
                     <br />
                     <ul>
-                        <li><span class="yellow">Layanan yang menggunakan koneksi internet (website, VPN endpoint, infrastruktur e-mail, extranet, dan lain-lain).</span></li>
-                        <li><span class="yellow">Sistem internal atau servis yang terdapat di dalam jaringan</li>
-                        <li><span class="yellow">Aplikasi mobile (iOs & Android ), web, dan desktop</span></li>
-                        <li><span class="yellow">Jaringan internal</span></li>
-                        <li><span class="yellow">Karyawan perusahaan</span></li>
-                        <li><span class="yellow">Stress testing</span></li>
+                        <li><span class="yellow">Layanan yang menggunakan koneksi internet (website, VPN endpoint,
+                                infrastruktur
+                                e-mail, extranet, dan lain-lain).</span></li>
+                        <li><span class="yellow">Sistem internal atau servis yang terdapat di dalam jaringan.</li>
+                        <li><span class="yellow">Aplikasi mobile (iOs & Android ), web, dan desktop.</span></li>
+                        <li><span class="yellow">Jaringan internal.</span></li>
+                        <li><span class="yellow">Karyawan perusahaan.</span></li>
+                        <li><span class="yellow">Stress testing.</span></li>
                     </ul>
                 </div>
             </div>
@@ -509,21 +468,15 @@
                         tersebut sebelum seseorang yang tidak bertanggung jawab mengambil kesempatan dari celah
                         kelemahan yang ditemukan. Dengan sistem keamanan yang baik, data-data sensitif perusahaan dapat
                         terlindungi sehingga perusahaan dapat terhindar dari sejumlah kerugian.
-                        <!-- <span class="title fw-300">Lalu mengapa <b>pentest</b> diperlukan?</span><br /> -->
-                        <!-- Pentest dilakukan sebenarnya untuk mengidentifikasi apakan terdapat kelemahan keamanan di dalam
-                        sebuah aplikasi, komputer atau jaringan. Jika kelemahan ditemukan dan dapat dibuktikan dengan
-                        beberapa analisa resikonya, maka Anda akan mempunyai waktu untuk dapat memperbaikinya sebelum
-                        seseorang yang tidak bertanggung jawab mengambil kesempatan dari kelemahan yang ditemukan
-                        tersebut. -->
                     </div>
                     <br /><br />
                     <div class="text-center lh-30">
-                        <span class="title fw-300">Apa kelebihan dan kekurangan pentest?</span><br />
-                        Teknik penetration testing mempunyai beberapa kelebihan dan kekurangan, antara lain yaitu:
+                        <span class="title fw-300">Apa saja kelebihan pentest?</span><br />
+                        Teknik penetration testing mempunyai beberapa kelebihan, antara lain yaitu:
                     </div>
                 </div>
                 <div class="clearfix"></div><br />
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <hr />
                     <div class="each-plus-minus">
                         <div class="title-each"><img src="assets/img/img-pentest/kelebihan.png"
@@ -535,18 +488,6 @@
                                     lebih rendah jika dibandingkan dengan melakukan teknik pengujian menggunakan
                                     pemeriksaan source code</span></li>
                             <li><span>Pengujian langsung dilakukan pada kode yang digunakan (exposed)</span></li>
-                        </ul>
-                    </div>
-                    <hr />
-                </div>
-                <div class="col-md-6">
-                    <hr />
-                    <div class="each-plus-minus">
-                        <div class="title-each"><img src="assets/img/img-pentest/kekurangan.png"
-                                alt="Kekurangan" />KEKURANGAN</div>
-                        <ul>
-                            <li><span>Terlalu lambat jika dilakukan dalam SDLC</span></li>
-                            <li><span>Hanya melakukan pengujian pada dampak depannya saja</span></li>
                         </ul>
                     </div>
                     <hr />
@@ -572,12 +513,6 @@
                         sertifikasi CEH (Certified Ethical Hacker) dan CSCU (Certified Secure Computer User) dari EC-
                         Council sehingga kemampuannya tidak perlu diragukan lagi.
                     </p>
-                    <!-- <p>
-                        Hal ini karena Logique Digital Indonesia mempunyai pentester yang telah berpengalaman dalam
-                        menemukan celah keamanan yang terdapat pada beberapa website rekanan bisnis mereka. Selain itu,
-                        pentester yang terdapat di Logique Digital Indonesia juga menerapkan standar operasional yang
-                        digunakan oleh semua pentester di seluruh dunia.
-                    </p> -->
 
                     <div class="title">Metode apa yang digunakan Logique Digital Indonesia dalam melakukan pentest?
                     </div>
@@ -712,38 +647,6 @@
                 </div>
             </div>
             <div class="hidden-xs"><br /></div>
-            <!-- <div class="row">
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <img alt="Step 5" src="assets/img/img-pentest/step5.png"
-                                class="img-responsive center-block" /><br />
-                        </div>
-                        <div class="col-sm-8 text-justify">
-                            <div class="title"><span>3</span> <b>Gaining Access</b></div>
-                            Pada fase ini, pentester perlu untuk mendapatkan akses untuk mengambil alih kendali dari
-                            satu atau lebih perangkat jaringan untuk selanjutnya mengekstrak data dari target, untuk
-                            selanjutnya menggunakan perangkat tersebut untuk meluncurkan serangan pada target lainnya.
-                            <br /><br />
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <img alt="Step 6" src="assets/img/img-pentest/step6.png"
-                                class="img-responsive center-block" /><br />
-                        </div>
-                        <div class="col-sm-8 text-justify">
-                            <div class="title"><span>4</span> <b>Maintaining Access</b></div>
-                            Yaitu tahapan dimana pentester akan membuat beberapa langkah-langkah yang diperlukan agar
-                            tetap berada di lingkungan target dengan tujuan untuk mengumpulkan data sebanyak mungkin.
-                            Pada fase ini, penyerang harus tetap dalam kondisi diam agar tidak dapat tertangkap ketika
-                            sedang menggunakan lingkungan host.<br /><br />
-                        </div>
-                    </div>
-                </div>
-            </div> -->
             <div class="hidden-xs"><br /></div>
             <div class="row">
                 <div class="bg-yellow">
@@ -785,70 +688,7 @@
 
         </div>
     </div>
-    <!-- <div class="mengapa-harus">Layanan <b>Keamanan Siber</b> yang terdapat di Logique:</div>
-	<br><br>
 
-	<div class="metode">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-offset-1 col-md-10">
-
-					<div class="row row-center">
-						<div class="col-sm-12">
-							<div class="subtitle fs-18 f-s-22"><b>A. Pentest Aplikasi</b></div>
-							Tes keamanan merupakan sebuah metode untuk mengevaluasi keamanan sistem komputer atau jaringan menggunakan metode validasi dan memverifikasi keefektifan dari kontrol keamanan aplikasi, seperti: <br><br>
-							<div class="p-l-3">
-							<b>1. Web</b><br>
-							Pengujian keamanan untuk aplikasi Web hanya berfokus pada mengevaluasi keamanan sebuah aplikasi web. Prosesnya menggunakan analisis aktif dari aplikasi tersebut untuk menemukan semua kelemahan, mulai dari kelemahan teknis hingga kerentanan yang bisa terjadi. Semua masalah yang ditemukan nantinya akan disajikan kepada pemilik sistem, bersamaan dengan penilaian dari dampak yang dapat ditimbulkan serta proposal rencana untuk mengurangi resiko tersebut atau biasa disebut sebagai solusi teknis.<br><br>
-							<b>2. Aplikasi Seluler</b><br>
-							Pengujian keamanan aplikasi seluler berfokus kepada evaluasi kemanan ponsel di perangkat iOS atau Android. Prosesnya menggunakan analisis aktif dari aplikasi tersebut guna menemukan semua kelemahan, seperti kelemahan teknis hingga kerentanan yang dapat terjadi. Semua masalah yang ditemukan pada aplikasi ini nantinya akan diberikan kepada pembuat sistem bersamaan dengan dampak yang akan ditimbulkan, proposal rencana untuk mengatasi hal tersebut atau biasa disebut dengan solusi teknis.<br><br>
-							<b>3. Aplikasi Desktop</b><br>
-							Banyak orang bertanya, “Apakah aplikasi Desktop masih digunakan?” Jawabannya adalah iya, masih digunakan. Meskipun saat ini telah ada aplikasi web dan aplikasi seluler, Namun aplikasi desktop tetap saja mempunyai kategori tersendiri. Bahkan sebagian besar perusahaan aplikasi berbasis instalasi masih tetap berjalan dengan tetap menggunakan sumber daya perangkat kerasnya. Selain itu, sistem realtime banyak yang masih berbasis desktop karena kemampuan kinerjanya yang terbilang mumpuni untuk membaca sistem tersebut. Kita juga dapat menguji aplikasi ini guna memastikan seluruh fungsi berjalan dengan baik dan benar. <br><br>Aplikasi desktop dapat digunakan baik di komputer pribadi maupun di komputer kerja Anda. Karena itu, lingkungan yang spesifik dapat membantu Anda untuk menentukan dasar rencana pengujian keamanan yang akan Anda lakukan atau dengan kata lain hanya dalam satu kali pengetesan Anda dapat melakukan pengujian secara lengkap, mulai dari grafis tampilan antarmuka, fungsi, serta beban aplikasi tersebut dan lain-lain. Sebuah aplikasi desktop biasanya digunakan oleh satu orang user pada satu waktu dan memerlukan penginstallan terlebih dahulu melalui file exe agar dapat dilakukan pengujian instalasi secara keseluruhan.
-							</div>
-						</div>
-					</div>
-					<hr/>
-					<div class="row row-center">
-						<div class="col-sm-12">
-							<div class="subtitle fs-18 f-s-22"><b>B. Pentest Jaringan dan Infrastruktur</b></div>
-							<div>
-							Melakukan pengujian pentest pada server, jaringan lokal serta pada komputer pribadi para staff merupakan salah satu metode pengujian infrastruktur yang terbilang efektif dan terbukti mampu mengevaluasi keamanan jaringan komputer Anda, mengetahui kelemahan infrastruktur dan aplikasi menggunakan simulasi malicious attack.
-							</div>
-						</div>
-					</div>
-					<hr/>
-					<div class="row row-center">
-						<div class="col-sm-12">
-							<div class="subtitle fs-18 f-s-22"><b>C. Peninjauan Kode</b></div>
-							<div>
-							Melakukan audit sumber kode aplikasi untuk mengetahui bahwa kontrol keamanan yang dilakukan telah tepat dan bekerja sesuai dengan fugnsinya masing-masing serta ditempatkan pada tempat yang tepat. Peninjauan kode dilakukan untuk memastikan bahwa aplikasi telah dikembangkan untuk dapat melakukan “pembelaan diri” sesuai dengan lingkungan yang dia hadapi.
-							</div>
-						</div>
-					</div>
-					<hr/>
-					<div class="row row-center">
-						<div class="col-sm-12">
-							<div class="subtitle fs-18 f-s-22"><b>D. Pelatihan Mengenai Pentingnya Keamanan Siber</b></div>
-							<div>
-							Melakukan pelatihan keamanan siber bagi para pengguna serta programmer tentu sangat penting. Keamanan siber terbaik tentunya tidak akan membuat Anda untuk memilih kata sandi yang lemah dan mengklik tautan yang buruk. Itulah mengapa Anda perlu pelatihan semacam ini.
-							</div>
-						</div>
-					</div>
-					<hr/>
-					<div class="row row-center">
-						<div class="col-sm-12">
-							<div class="subtitle fs-18 f-s-22"><b>E. Konsultasi Keamanan Siber</b></div>
-							<div>
-							Kami siap membantu tim IT klien kami untuk memecahkan masalah keamanan (memberikan solusi terbaik). Kami dapat memberikan saran teknis dan saran strategis kepada tim IT untuk memanajemen tim Anda mengenai keamanan siber tersebut.
-							</div>
-						</div>
-					</div>
-					<br>
-					<br>
-				</div>
-			</div>
-		</div>
-	</div> -->
 
     <div class="harga">
         <div class="container">
