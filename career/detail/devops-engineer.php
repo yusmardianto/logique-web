d49a463191d25df9a3995654bbdbfa78bf9bf03d
Melvin Marzuki <44135848+melvinmarz@users.noreply.github.com>
2019-11-27 11:47:06 +0700

    Added 2 scripts on web-security karir page and devops-engineer karir page

--- career/detail/devops-engineer.php
+++ career/detail/devops-engineer.php
@@ -87,6 +87,15 @@
 </noscript>
 <!-- End Facebook Pixel Code -->
 
+<!-- Google Tag Manager -->
+<script>
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+  var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KQJRS4V');
+</script>
+<!-- End Google Tag Manager -->
+
   <!-- Bootstrap -->
   <link href="/css/bootstrap.min.css" rel="stylesheet">
   <link href="/css/style.css" rel="stylesheet">
@@ -348,6 +357,12 @@
     </script>
 
   </div>
+
+  <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQJRS4V" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+  <!-- End Google Tag Manager (noscript) -->
 </body>
 
 </html>
