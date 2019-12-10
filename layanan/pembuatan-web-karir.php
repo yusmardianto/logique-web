0ca05d114a90fc4f7277f7c6c8c77c4389711296
Bagas Ranggen <bagas@logique.co.id>
2019-12-10 13:28:02 +0700

    update fe: - recruit web

--- layanan/pembuatan-web-karir.php
+++ layanan/pembuatan-web-karir.php
@@ -63,23 +63,23 @@ if(isset($_POST['submitted']))
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
     <script>
-    // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    // ga('create', 'UA-105878648-1', 'auto');
-    // ga('send', 'pageview');
+    ga('create', 'UA-105878648-1', 'auto');
+    ga('send', 'pageview');
     </script>
     <!-- Twitter universal website tag code -->
-    <!-- <script>
+    <script>
 	!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
 	},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
 	a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
 	// Insert Twitter Pixel ID and Standard Event data below
 	twq('init','o0xru');
 	twq('track','PageView');
-	</script> -->
+	</script>
     <!-- End Twitter universal website tag code -->
 </head>
 
