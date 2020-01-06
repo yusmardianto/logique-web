a1efe8760408353e282729a67b027f7ab360c0a9
Faiq Naufal <nauval@logique.co.id>
2020-01-06 22:44:20 +0700

    created starter template for career landing page update

---
+++ career/starter-career.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0">
+    <meta name="description" itemprop="description"
+        content="Kami memberikan kesempatan berkarir bagi para web developer, web designer, dan digital marketing untuk terus mengasah kemampuannya dengan bergabung bersama LOGIQUE Digital Indonesia. Segera bergabunglah bersama kami!">
+    <meta name="keywords"
+        content="keuntungan bekerja di logique, join us, web developer, lowongan kerja, digital marketing, seo, desain">
+    <meta name="author" content="PT. Logique Digital Indonesia">
+    <meta property="og:title" content="Karir | Info Lowongan Kerja | LOGIQUE Digital Indonesia">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Logique">
+    <meta property="og:url" content="https://www.logique.co.id/career/">
+    <meta property="og:image" content="https://www.logique.co.id/img/og-image-update.png">
+    <meta property="og:description"
+        content="Saatnya berkarir dan kembangkan potensimu - Career at Logique Bergabunglah dan jadilah bagian dari perkembangan teknologi digital di Indonesia bersama Logique. Anda juga akan memiliki kesempatan luas untuk mengembangkan potensi dan karir Anda, serta berkarya dengan jam kerja yang fleksibel.">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <title>Lowongan</title>
+
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/career/style-career.css" rel="stylesheet">
+    <!-- <link href="/css/style.css" rel="stylesheet"> -->
+    <link href="/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/aos.css" rel="stylesheet">
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-KQJRS4V');
+    </script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Facebook Pixel Code -->
+    <script>
+    ! function(f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function() {
+            n.callMethod ?
+                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = '2.0';
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '713777762291663');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=713777762291663&ev=PageView&noscript=1" /></noscript>
+    <!-- End Facebook Pixel Code -->
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQJRS4V" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <div class="wrapper">
+
+        <!-- NAVIGATION -->
+        <button type="button" class="navbar-toggle collapsed navbar-toggle--update-design" aria-label="navbar-toggle"
+            data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar first"></span>
+            <span class="icon-bar second"></span>
+            <span class="icon-bar third"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <div class="backdrop-test"></div>
+            <div class="logique-nav">
+                <div class="margin-for-nav">
+                    <div class="text-center">
+                        <a href="/index.php">
+                            <div class="img-home"></div>
+                            Beranda
+                        </a>
+                    </div>
+                    <div class="text-center">
+                        <a href="/logique.php">
+                            <div class="img-about"></div>
+                            Tentang Kami
+                        </a>
+                    </div>
+                    <div class="text-center">
+                        <a href="/portofolio.php">
+                            <div class="img-portfolio"></div>
+                            Portfolio
+                        </a>
+                    </div>
+                    <div class="text-center">
+                        <a href="/layanan.php">
+                            <div class="img-services"></div>
+                            Layanan
+                        </a>
+                    </div>
+                    <div class="text-center">
+                        <a href="/produk.php">
+                            <div class="img-product"></div>
+                            Produk
+                        </a>
+                    </div>
+                    <div class="text-center active">
+                        <a href="#">
+                            <div class="img-career"></div>
+                            Karir
+                        </a>
+                    </div>
+                    <div class="text-center">
+                        <a href="/hubungi-kami.php">
+                            <div class="img-contact"></div>
+                            Hubungi Kami
+                        </a>
+                    </div>
+                    <div class="text-center">
+                        <a href="https://www.logique.co.id/blog/" target="_blank" rel="noreferrer">
+                            <div class="img-blog"></div>
+                            Blog
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- NAVIGATION -->
+
+        <!-- HEADER -->
+        <div class="container-fluid">
+            <div class="lang-logo--update-design">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-offset-1 col-sm-8">
+                            <a href="/index.php"><img src="../../img/logo.png"
+                                        alt="Jasa Pembuatan Website Logique Digital Indonesia"
+                                        class="img-responsive logo--update-design" /></a>
+                        </div>
+                        <div class="col-sm-3">
+                                <a href="#" class="link-header-lamar-lowongan">
+                                    Melamar Lowongan Sekarang
+                                </a>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <!-- END HEADER -->
+
+        <div class="career-update content-wrapper__">
+            <section class="csection bg--white --top">
+                <div class="container__">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempora, numquam sapiente amet debitis cumque! Et perspiciatis consectetur minima nemo nam tempore repellendus aliquam, delectus fugiat, sequi eligendi perferendis sit!
+                </div>
+            </section>
+        </div>
+
+        <?php include '../footer.php'; ?>
+    </div>
+    <script type="text/JavaScript">
+        $('.waNumber').html('0822-60-5678-56');
+    </script>
+</body>
+
+</html>
