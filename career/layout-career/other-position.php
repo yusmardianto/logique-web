0ec3929f5a4f70eaa8bb9af68ecdca8b4a9f9043
Bagas Ranggen <bagas@logique.co.id>
2020-01-10 12:52:30 +0700

    update fe: - finishing basic structure of career detail - finishing reusable components - add new assets

--- career/layout-career/other-position.php
+++ career/layout-career/other-position.php
@@ -4,7 +4,7 @@
             <div class="col-sm-offset-1 col-sm-10 no-gap">
                 <h2 class="section-title">See Position in Logique</h2>
                 <div class="row see-position">
-                    <div class="col-sm-6 col-md-4">
+                    <div class="col-md-6 col-lg-4">
                         <a href="#" class="see-position__item bg--generalist">
                             <div class="section-title__wrapper">
                                 <h3 class="section-title">IT Generalist</h3>
@@ -12,7 +12,7 @@
                             <p>Project Manager, Product Manager, IT Consultant, Business Analyst, etc,.</p>
                         </a>
                     </div>
-                    <div class="col-sm-6 col-md-4">
+                    <div class="col-md-6 col-lg-4">
                         <a href="#" class="see-position__item bg--programmer">
                             <div class="section-title__wrapper">
                                 <h3 class="section-title">Programmer</h3>
@@ -20,7 +20,7 @@
                             <p>Project Manager, Product Manager, IT Consultant, Business Analyst, etc,.</p>
                         </a>
                     </div>
-                    <div class="col-sm-6 col-md-4">
+                    <div class="col-md-6 col-lg-4">
                         <a href="#" class="see-position__item bg--design">
                             <div class="section-title__wrapper">
                                 <h3 class="section-title">Desainer/ <br>Digital Marketing</h3>
@@ -28,7 +28,7 @@
                             <p>Project Manager, Product Manager, IT Consultant, Business Analyst, etc,.</p>
                         </a>
                     </div>
-                    <div class="col-sm-6 col-md-4">
+                    <div class="col-md-6 col-lg-4">
                         <a href="#" class="see-position__item bg--specialist">
                             <div class="section-title__wrapper">
                                 <h3 class="section-title">Other IT Specialist</h3>
@@ -36,7 +36,7 @@
                             <p>Project Manager, Product Manager, IT Consultant, Business Analyst, etc,.</p>
                         </a>
                     </div>
-                    <div class="col-sm-6 col-md-4">
+                    <div class="col-md-6 col-lg-4">
                         <a href="#" class="see-position__item bg--sales">
                             <div class="section-title__wrapper">
                                 <h3 class="section-title">Sales & Marketing</h3>
@@ -44,7 +44,7 @@
                             <p>Project Manager, Product Manager, IT Consultant, Business Analyst, etc,.</p>
                         </a>
                     </div>
-                    <div class="col-sm-6 col-md-4">
+                    <div class="col-md-6 col-lg-4">
                         <a href="#" class="see-position__item bg--bo" style="--image-url(../../img/career/bg/bg-bo.png);">
                             <div class="section-title__wrapper">
                                 <h3 class="section-title">Back Office</h3>
--- career/layout-career/other-vacancy.php
+++ career/layout-career/other-vacancy.php
@@ -4,55 +4,63 @@
     $cat_vacancy = $arr_url[3]; //category vacancy
 ?>
 <section class="container-fluid ">
-  <div class="container--max-width">
-    <div class="row">
-      <div class="col-sm-offset-1 col-sm-10 no-gap">
-        <h2 class="section-title">Other Position</h2>
-        <div class="row other-position">
-          <?php if ($cat_vacancy !== 'it-generalist'): ?>
-          <div class="col-sm-6 col-md-4">
-            <a href="#" class="other-position__item bg--generalist">
-              <h3 class="section-title">IT Generalist</h3>
-            </a>
-          </div>
-          <?php endif; ?>
-          <?php if ($cat_vacancy !== 'programmer'): ?>
-          <div class="col-sm-6 col-md-4">
-            <a href="#" class="other-position__item bg--programmer">
-              <h3 class="section-title">Programmer</h3>
-            </a>
-          </div>
-          <?php endif; ?>
-          <?php if ($cat_vacancy !== 'desainer-marketing'): ?>
-          <div class="col-sm-6 col-md-4">
-            <a href="#" class="other-position__item bg--design">
-              <h3 class="section-title">Desainer / Digital Marketing</h3>
-            </a>
-          </div>
-          <?php endif; ?>
-          <?php if ($cat_vacancy !== 'it-specialist'): ?>
-          <div class="col-sm-6 col-md-4">
-            <a href="#" class="other-position__item bg--specialist">
-              <h3 class="section-title">IT Specialist</h3>
-            </a>
-          </div>
-          <?php endif; ?>
-          <?php if ($cat_vacancy !== 'sales-back-office'): ?>
-          <div class="col-sm-6 col-md-4">
-            <a href="#" class="other-position__item bg--sales">
-              <h3 class="section-title">Sales / Back Office</h3>
-            </a>
-          </div>
-          <?php endif; ?>
-          <?php if ($cat_vacancy !== 'sales-back-office'): ?>
-          <div class="col-sm-6 col-md-4">
-            <a href="#" class="other-position__item bg--generalist">
-              <h3 class="section-title">Internship</h3>
-            </a>
-          </div>
-          <?php endif; ?>
+    <div class="container--max-width">
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-10 no-gap">
+                <h2 class="section-title">Other Position</h2>
+                <div class="row other-position">
+                    <?php if ($cat_vacancy !== 'it-generalist'): ?>
+                    <div class="col-md-6 col-lg-4">
+                        <a href="#" class="other-position__item bg--generalist">
+                            <!-- <div class="other-position__overlay"></div> -->
+                            <h3 class="section-title">IT Generalist</h3>
+                        </a>
+                    </div>
+                    <?php endif; ?>
+                    <?php if ($cat_vacancy !== 'programmer'): ?>
+                    <div class="col-md-6 col-lg-4">
+                        <a href="#" class="other-position__item bg--op-programmer">
+                            <div class="other-position__overlay"></div>
+                            <h3 class="section-title">Programmer</h3>
+                        </a>
+                    </div>
+                    <?php endif; ?>
+                    <?php if ($cat_vacancy !== 'desainer-marketing'): ?>
+                    <div class="col-md-6 col-lg-4">
+                        <a href="#" class="other-position__item bg--op-designer">
+                            <div class="other-position__overlay"></div>
+                            <span class="other-position__urgent">Urgently Needed</span>
+                            <h3 class="section-title">Desainer / Digital Marketing</h3>
+                        </a>
+                    </div>
+                    <?php endif; ?>
+                    <?php if ($cat_vacancy !== 'it-specialist'): ?>
+                    <div class="col-md-6 col-lg-4">
+                        <a href="#" class="other-position__item bg--op-specialist">
+                            <div class="other-position__overlay"></div>
+                            <h3 class="section-title">IT Specialist</h3>
+                        </a>
+                    </div>
+                    <?php endif; ?>
+                    <?php if ($cat_vacancy !== 'sales-back-office'): ?>
+                    <div class="col-md-6 col-lg-4">
+                        <a href="#" class="other-position__item bg--op-sales">
+                            <div class="other-position__overlay"></div>
+                            <span class="other-position__urgent">Urgently Needed</span>
+                            <h3 class="section-title">Sales / Back Office</h3>
+                        </a>
+                    </div>
+                    <?php endif; ?>
+                    <?php if ($cat_vacancy !== 'sales-back-office'): ?>
+                    <div class="col-md-6 col-lg-4">
+                        <a href="#" class="other-position__item bg--op-internship">
+                            <div class="other-position__overlay"></div>
+                            <h3 class="section-title">Internship</h3>
+                        </a>
+                    </div>
+                    <?php endif; ?>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </section>
