dda8f3d30ef529bd55be3687480dede45f027018
Faiq Naufal <nauval@logique.co.id>
2020-01-16 11:03:21 +0700

    created detail position page of frontend developer, react-native developer, reactjs developer, and web developer

---
+++ career/lowongan/programmer/frontend-developer.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0">
+  <meta name="description" itemprop="description"
+    content="Kami memberikan kesempatan berkarir bagi para Front End Developer, web designer, dan digital marketing untuk terus mengasah kemampuannya dengan bergabung bersama LOGIQUE Digital Indonesia. Segera bergabunglah bersama kami!">
+  <meta name="keywords"
+    content="keuntungan bekerja di logique, join us, Front End Developer, lowongan kerja, digital marketing, seo, desain">
+  <meta name="author" content="PT. Logique Digital Indonesia">
+  <meta property="og:title" content="Karir | Info Lowongan Kerja | LOGIQUE Digital Indonesia">
+  <meta property="og:type" content="website">
+  <meta property="og:site_name" content="Logique">
+  <meta property="og:url" content="https://www.logique.co.id/career/">
+  <meta property="og:image" content="https://www.logique.co.id/img/og-image-update.png">
+  <meta property="og:description"
+    content="Saatnya berkarir dan kembangkan potensimu - Career at Logique Bergabunglah dan jadilah bagian dari perkembangan teknologi digital di Indonesia bersama Logique. Anda juga akan memiliki kesempatan luas untuk mengembangkan potensi dan karir Anda, serta berkarya dengan jam kerja yang fleksibel.">
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+  <title>Front End Developer</title>
+
+  <!-- Bootstrap -->
+  <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/career/style-career.css" rel="stylesheet">
+  <!-- <link href="/css/style.css" rel="stylesheet"> -->
+  <link href="/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="/css/aos.css" rel="stylesheet">
+  <!-- Google Tag Manager -->
+  <script>
+  (function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-KQJRS4V');
+  </script>
+  <!-- End Google Tag Manager -->
+
+  <!-- Facebook Pixel Code -->
+  <script>
+  ! function(f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function() {
+      n.callMethod ?
+        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+  }(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '713777762291663');
+  fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=713777762291663&ev=PageView&noscript=1" /></noscript>
+  <!-- End Facebook Pixel Code -->
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQJRS4V" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <div class="wrapper">
+
+    <!-- Header -->
+    <?php include '../../layout-career/header-career.php'; ?>
+
+    <div class="career-update content-wrapper__">
+
+      <section class="container-fluid --top bg--project-manager">
+        <div class="container--max-width header-banner">
+          <div class="row">
+            <div class="col-sm-offset-1 col-sm-10">
+              <ol class="breadcrumb breadcrumb--update-design" itemscope itemtype="http://schema.org/BreadcrumbList">
+                <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item"
+                    href="/index.php"><span itemprop="name">Beranda</span></a>
+                  <meta itemprop="position" content="1" />
+                </li>
+                <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item"
+                    href="/career/index.php"><span itemprop="name">&nbsp; Karir</span></a>
+                  <meta itemprop="position" content="2" />
+                </li>
+                <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item"
+                    href="/career/lowongan/programmer.php"><span itemprop="name">&nbsp; Programmer</span></a>
+                  <meta itemprop="position" content="3" />
+                </li>
+                <li class="active" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a
+                    itemprop="item"><span itemprop="name">&nbsp; Front End Developer</span></a>
+                  <meta itemprop="position" content="4" />
+                </li>
+              </ol>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-offset-1 col-lg-6 col-md-9 col-sm-10">
+              <h1 class="header__title">Front End Developer</h1>
+              <div class="career-detail">
+                <div class="career-detail__item">
+                  <img class="career-detail__img" src="/img/career/icon/work.png" alt="Work Type">
+                  <p>Programmer</p>
+                </div>
+                <div class="career-detail__item">
+                  <img class="career-detail__img" src="/img/career/icon/time.png" alt="Contract Type">
+                  <p>Permanent</p>
+                </div>
+                <div class="career-detail__item">
+                  <img class="career-detail__img" src="/img/career/icon/location.png" alt="Work Location">
+                  <p>Jakarta, Yogyakarta, remote working</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="container-fluid">
+        <div class="container--max-width">
+          <div class="row">
+            <div class="col-sm-offset-1 col-md-5 col-sm-10">
+              <div class="career-requirement">
+                <h3 class="section-title">Job Description</h3>
+                <ul class="career-list">
+                  <li>
+                    Memanfaatkan HTML, CSS, Javascript, atau bahasa pemrograman terkait tampilan lainnya untuk mengembangkan antarmuka pengguna yang efektif pada website dan aplikasi.
+                  </li>
+                  <li>
+                    Bekerja sama dan berkolaborasi dengan tim developer dan desainer untuk membuat, memperbarui, atau memperbaiki UI/UX website dan aplikasi.
+                  </li>
+                  <li>
+                    Memberikan kualitas hasil terbaik dan tepat waktu sesuai dengan teknologi UI/UX terkini dan/atau yang sesuai dengan kebutuhan proyek.
+                  </li>
+                  <li>
+                    Memastikan kompatibilitas browser atau ukuran layar pada halaman-halaman website dan aplikasi yang dihasilkan.
+                  </li>
+                  <li>
+                    Membuat kode yang terstruktur dan dapat digunakan kembali untuk kemudahan pengembangan atau perubahan di masa depan.
+                  </li>
+                  <li>
+                    Memonitor dan mengoptimalkan performa website atau aplikasi dari sisi front-end.
+                  </li>
+                  <li>
+                    Berkomunikasi langsung dengan klien terkait UI/UX jika diperlukan.
+                  </li>
+                </ul>
+              </div>
+              <div class="career-requirement">
+                <h3 class="section-title">Persyaratan</h3>
+                <ul class="career-list">
+                  <li>
+                   Berpendidikan minimal D3 dan berpengalaman minimal 2 tahun di bidang IT atau programmer.
+                  </li>
+                  <li>
+                    Berpengalaman dalam pembuatan WEB/Sistem.
+                  </li>
+                  <li>
+                    Pengalaman dalam coding PSD ke HTML-CSS.
+                  </li>
+                  <li>
+                    Mampu membuat responsive website.
+                  </li>
+                  <li>
+                    Mengenal proses pengembangan website dengan baik.
+                  </li>
+                  <li>
+                    Dapat belajar secara cepat dan mandiri.
+                  </li>
+                  <li>
+                    Mampu bekerja dengan tim antar divisi.
+                  </li>
+                  <li>
+                    Mampu beradaptasi dengan cepat dalam penggunaan teknologi baru
+                  </li>
+                  <li>
+                    Memahami basic bahasa pemrograman PHP.
+                  </li>
+                  <li>
+                    HTML 5
+                  </li>
+                  <li>
+                    CSS 3 ( Bootstrap, Foundation, Material UI )
+                  </li>
+                  <li>
+                    JavaScript ( JQuery)
+                  </li>
+                </ul>
+              </div>
+
+              <div class="career-requirement">
+                <h3 class="section-title">Preferable</h3>
+                <ul class="career-list">
+                  <li>
+                    Additional Skill: React JS, Ionic, Angular, Node JS.
+                  </li>
+                  <li>
+                    Mempunyai minat yang besar terhadap bisnis Internet dan Web.
+                  </li>
+                  <li>
+                    Memahami pembuatan template wordpress.
+                  </li>
+                </ul>
+              </div>
+
+              <div class="career-requirement">
+                <h3 class="section-title">Rewards and Worthwhile</h3>
+                <ul class="career-list">
+                  <li>
+                    Menambah pengetahuan dan pengalaman tentang technology baru dalam front end development.
+                  </li>
+                  <li>
+                    Salary Negotiable (tergantung pada skill, potensi, pengalaman dan gaji sebelumnya).
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-offset-1 col-md-offset-0 col-md-5 col-sm-10">
+              <!-- <div class="career-requirement">
+                <h3 class="section-title">How a Front End Developer in LOGIQUE works usually</h3>
+                <h4 class="section-subtitle">Front End Developer</h4>
+                <div class="work-schedule">
+                  <div class="work-schedule__item">
+                    <div class="work-schedule__time">10:00 – 10:10</div>
+                    <div class="work-schedule__text">Daily Scrum Meeting</div>
+                  </div>
+                  <div class="work-schedule__item">
+                    <div class="work-schedule__time">10:10 – 10:30</div>
+                    <div class="work-schedule__text">Check Trello, Git and Slack, and confirm “to do” today</div>
+                  </div>
+                  <div class="work-schedule__item">
+                    <div class="work-schedule__time">10:30 – 12:00</div>
+                    <div class="work-schedule__text">Programming for product A</div>
+                  </div>
+                  <div class="work-schedule__item">
+                    <div class="work-schedule__time">12:00 – 13:30</div>
+                    <div class="work-schedule__text">Istirahat makan siang</div>
+                  </div>
+                  <div class="work-schedule__item">
+                    <div class="work-schedule__time">13:30 – 15:00</div>
+                    <div class="work-schedule__text">Internal meeting dengan product manager mengenai product Logique , laporan progress, membuat target yang baru</div>
+                  </div>
+                  <div class="work-schedule__item">
+                    <div class="work-schedule__time">15:00 – 16:00</div>
+                    <div class="work-schedule__text">Programming / DevOps untuk product logique</div>
+                  </div>
+                  <div class="work-schedule__item">
+                    <div class="work-schedule__time">16:00</div>
+                    <div class="work-schedule__text">Pulang</div>
+                  </div>
+                  <div class="work-schedule__item">
+                    <div class="work-schedule__time">19:00 – 21:00</div>
+                    <div class="work-schedule__text">Continue Programming for product A
+                    </div>
+                  </div>
+                </div>
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- <section class="container-fluid bg--salary">
+        <div class="container--max-width">
+          <div class="row">
+            <div class="col-sm-offset-1 col-sm-10">
+              <h3 class="section-title">Salary Range</h3>
+              <div class="row">
+                <div class="col-md-4 col-sm-12">
+                  <div class="salary-range">
+                    <div class="salary-range__head">
+                      <div class="salary-range__head-top">
+                        <em class="salary-range__position">Junior</em>
+                        <span class="salary-range__salary">〜 100 juta IDR / Year</span>
+                      </div>
+                      <h5 class="section-title">Product <br>Developer:</h5>
+                    </div>
+                    <ul class="salary-range__list">
+                      <li>Can program / code at least one programming language (PHP or JavaScript)</li>
+                      <li>Can code by following the coding guide without many bugs</li>
+                      <li>Can read programming source that others coded and update them</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                  <div class="salary-range">
+                    <div class="salary-range__head">
+                      <div class="salary-range__head-top">
+                        <em class="salary-range__position">Middle</em>
+                        <span class="salary-range__salary">100 〜 200 juta IDR / Year</span>
+                      </div>
+                      <h5 class="section-title">Product <br>Developer:</h5>
+                    </div>
+                    <ul class="salary-range__list">
+                      <li>Can program / code or know some programming languages (at least both PHP and JavaScript)                        
+                      </li>
+                      <li>Can catch up new technology and new things quickly</li>
+                      <li>Can deal with Database well, and should have infrastructure knowledge somehow</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                  <div class="salary-range">
+                    <div class="salary-range__head">
+                      <div class="salary-range__head-top">
+                        <em class="salary-range__position">Senior</em>
+                        <span class="salary-range__salary">200 juta IDR 〜 / Year</span>
+                      </div>
+                      <h5 class="section-title">Product <br>Manager:</h5>
+                    </div>
+                    <ul class="salary-range__list">
+                      <li>Have a system architect and design knowledge and good at DevOps</li>
+                      <li>Good coding knowledge and can lead other developers</li>
+                      <li>Have a high ability of solving a problem and Code fast</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section> -->
+
+      <section class="container-fluid bg--career-apply" style="margin-top:2.5em;">
+        <div class="container--max-width">
+          <div class="row">
+            <div class="col-sm-offset-2 col-sm-8 apply-now">
+              <div class="apply-now__item">
+                <h5 class="apply-now__text">Talented and have a motivation to grow and be promoted very fast?</h5>
+              </div>
+              <div class="apply-now__item text-center">
+                <div class="btn-banner-pattern__wrapper">
+                  <a class="btn-banner-pattern" href="#">
+                    <img class="img-responsive" src="/img/career/bg-img/bg-lowongan.png"
+                      alt="Melamar Lowongan Sekarang">
+                    <span>Melamar Lowongan Sekarang</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <?php include '../../layout-career/news-recruitment-topic.php'; ?>
+      <?php include '../../layout-career/other-vacancy.php'; ?>
+      <?php include '../../layout-career/link-section.php'; ?>
+      <?php include '../../layout-career/contact-lgq.php'; ?>
+
+    </div>
+
+    <?php include '../../../footer.php'; ?>
+  </div>
+  <script type="text/JavaScript">
+    $('.waNumber').html('0822-60-5678-56');
+    </script>
+</body>
+
+</html>
