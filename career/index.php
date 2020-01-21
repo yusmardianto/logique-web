295964fafa7277cb62053f381b4d3ad5be293d0b
Bagas Ranggen <bagas@logique.co.id>
2020-01-21 14:52:53 +0700

    update fe - career: - remove popup

--- career/index.php
+++ career/index.php
@@ -78,7 +78,6 @@
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQJRS4V" height="0" width="0"
             style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
-    <?php include '../popup.php'; ?>
     <div class="wrapper">
 
         <!-- NAVIGATION -->
