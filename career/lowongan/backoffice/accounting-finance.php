31706cc2d246bdf1825a9be9db9033d89a1048fb
Anggoro Kemarn <anggo@logique.co.id>
2020-08-07 14:57:20 +0700

    update text

--- career/lowongan/backoffice/accounting-finance.php
+++ career/lowongan/backoffice/accounting-finance.php
@@ -191,7 +191,7 @@
                 </ul>
               </div>
               <div class="career-requirement">
-                <h3 class="section-title">How an Akunting & Finance in LOGIQUE works usually</h3>
+                <h3 class="section-title">Cara Bekerja Akunting & Finance Sehari - hari di <strong class="logofont">LOGIQUE</strong></h3>
                 <!-- <h4 class="section-subtitle">Akunting & Finance Assistant</h4> -->
                 <div class="work-schedule">
                   <div class="work-schedule__item">
