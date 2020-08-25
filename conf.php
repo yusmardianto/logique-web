69e073e4bd1a5d9b410703bdc7a2d8b5084d6730
Nur Muhammad Abdul Falaq <falaq@logique.co.id>
2020-08-25 20:33:17 +0700

    update proses white paper untuk web dev 1

---
+++ conf.php
@@ -0,0 +1,12 @@
+<?php
+$customerDBName = "white_paper";
+$mysqli = mysqli_connect('localhost','root','','white_paper');
+$jasper_path     = '/home/dev/public_html/static/logique-web/';
+
+$base_url           = "http://logique-web.static.logique.co.id/";
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
