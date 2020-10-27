6555867c2f1d30d420046b83907d70cf3a015d17
Anggoro Kemarn <anggo@logique.co.id>
2020-10-27 17:27:50 +0700

    fixng css

--- jp/services/mobile-app-dev.php
+++ jp/services/mobile-app-dev.php
@@ -80,7 +80,7 @@ if(isset($_POST['submitted']))
      </script>
  </head>
 
- <body class="wrapper wrapper--update-design mobileapp-page">
+ <body class="  wrapper--update-design mobileapp-page">
      <style>
      .container__ {
          padding-top: 0;
@@ -173,7 +173,7 @@ if(isset($_POST['submitted']))
 
      <!-- HEADER -->
      <div class="container-fluid">
-         <div class="row lang-logo--update-design">
+         <div class="  lang-logo--update-design">
              <div class="container">
                  <div class="row">
                      <div class="col-sm-offset-1 col-sm-11">
