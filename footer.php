bf8e06375e3934bc7a720df6c8f556bd8fb980b9
Bagas Ranggen <bagas@logique.co.id>
2020-11-03 15:33:54 +0700

    change jquery

--- footer.php
+++ footer.php
@@ -164,8 +164,8 @@
     twq('init', 'o0xru');
     twq('track', 'PageView');
     </script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- <script   src="/js/jquery-3.min.js"></script> -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+<script   src="/js/jquery-3.min.js"></script>
 <script    src="/js/bootstrap.min.js"></script>
 <script  async src="/js/slick.min.js"></script>
 
