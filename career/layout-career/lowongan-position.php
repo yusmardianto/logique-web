42dfed7e2334e4e5f43b0372769000b1836ac399
fahmi123 <fahmiahmadi123@gmail.com>
2021-03-17 17:29:48 +0700

    update project-manager-lead page and project-manager page

--- career/layout-career/lowongan-position.php
+++ career/layout-career/lowongan-position.php
@@ -15,7 +15,7 @@
               class="lowongan-position__item bg--sub-project-manager">
               <div class="lowongan-position__overlay"></div>
               <span class="other-position__urgent">Urgently Needed</span>
-              <h3 class="section-title">Project Manager Lead</h3>
+              <h3 class="section-title">Consultant / <br>PM Lead</h3>
             </a>
           </div>
           <div class="col-md-6 col-lg-4">
