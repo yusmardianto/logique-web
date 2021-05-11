d73828bbff05771485a650e76419a88a2825a271
Abdul Rachman <6656642-abdul_rachman@users.noreply.gitlab.com>
2021-05-11 09:15:29 +0000

    Feat/faq section

--- layanan/harga-pembuatan-website.php
+++ layanan/harga-pembuatan-website.php
@@ -103,6 +103,16 @@ if (isset($_POST['submitted'])) {
 
 </head>
 
+<!-- <style>
+    @media only screen and (max-width: 768px) {
+    
+
+        .d-title-large {
+            font-size: larger;
+        }
+    }
+</style> -->
+
 <body>
 
     <?php
@@ -692,10 +702,246 @@ if (isset($_POST['submitted'])) {
                     <img loading="lazy" src="/layanan/assets/img/img-webdev/linkedinads.png" alt="linkedin banner">
                 </div>
             </section>
+            
+             <!-- FAQ -->
+            <section class="plan plan-gap">
+            <div class="container__">
+                <div style="margin-top: 1em;">
+                    <div class="clearfix ">                           
+                        <dt class="title-header" style="text-align: left;">Pertanyaan yang sering diajukan:
+                        </dt>
+                        <hr class="col-sm-1 col-xs-2 bar-yellow" style="padding: 5px 0; border: none;">
+                        
+                    </div>
+                    <div class="mt-12" id="accordion" style="width:100%;">
 
+                        <div class="panel-group" id="accordion">
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse1">
+                                    <dt class="panel-title">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>Saya ingin mengetahui rentang harga
+                                            layanan
+                                            LOGIQUE.</a>
+                                    </dt>
+                                </div>
+                                <div id="collapse1" class="panel-collapse collapse in">
+                                    <div class="panel-body">
+                                        Harga layanan tergantung pada kebutuhan Anda. Untuk melihat kisaran harga untuk
+                                        pengembangan Website,
+                                        kunjungilah halaman ini: <a href="https://www.logique.co.id/layanan/jasa-pembuatan-website.php" target="blank"><strong><i>Kisaran Harga Pengembangan Web LOGIQUE</i></strong></a>.
+                                        Harap dicatat bahwa Informasi yang tertera hanyalah kisaran umum.
+                                        Kami akan memberikan perkiraan yang lebih terperinci setelah mengetahui kebutuhan
+                                        Anda.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse2">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>BIsakah LOGIQUE mengirimkan beberapa
+                                            programmer untuk bekerja onsite?</a>
+                                    </dt>
+                                </div>
+                                <div id="collapse2" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Kami biasanya mengembangkan Web / Sistem / Aplikasi secara offsite (di pusat
+                                        pengembangan
+                                        kami sendiri).
+                                        Tetapi jika bekerja secara onsite merupakan persyaratan wajib atau jika ada alasan
+                                        yang
+                                        masuk akal / tak terhindarkan,
+                                        kita bisa bertindak fleksibel. Tolong jangan ragu untuk berkonsultasi.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse3">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Kami juga sebuah perusahaan pengembang. Bisakah LOGIQUE menjadi sub-kontraktor
+                                            kami?
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse3" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Tergantung situasi. Kami pernah bekerja sama dengan perusahaan konsultasi,
+                                        vendor perangkat lunak, agen IT in-house dan biro iklan. Kami terbukapada peluang
+                                        dan tantangan baru.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse4">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Apakah proses proyek dapat dijalankan dalam Bahasa Inggris?
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse4" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Bisa. Sebagian besar dari staf kami dapat berkomunikasi dalam Bahasa Inggris.
+                                        Kami pernah bekerja dengan klien Jepang, Singapura, Malaysia, Filipina, Australia,
+                                        Inggris, Amerika, Swedia, dan Italia.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse5">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Apakah meeting dapat dilakukan di luar negeri?
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse5" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Mari kita bahas masalah ini dengan Anda terlebih dahulu. Biasanya kami menawarkan
+                                        video
+                                        conference call,
+                                        tetapi kami bisa memenuhi hal tersebut tergantung pada kasusnya.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse6">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Kami sedang berpikir untuk mengganti agen outsourcing web / sistem kami saat ini
+                                            dengan LOGIQUE.
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse6" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Silakan hubungi kami tanpa ragu-ragu. Kami mampu mengembangkan berbagai produk digital
+                                        melalui pengalaman bertahun-tahun di bidang pengembangan sistem manajemen pelanggan.
+                                        Kami juga mampu mengembangkan sistem untuk meningkatkan rekrutmen,
+                                        e-commerce, manajemen poin, dan akuntansi dan keuangan, di antara banyak hal lainnya.
+                                        Kami
+                                        ahli dalam pengembangan sistem, memanfaatkan teknologi open web secara maksimal.
+                                        Silakan hubungi kami untuk keterangan lebih lanjut.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse7">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Kami ingin mengembangkan sistem managemen untuk bisnis kami.
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse7" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Kami mampu mengembangkan berbagai jenis sistem karena kami memiliki pengalaman di
+                                        bidang
+                                        pengembangan sistem manajemen pelanggan,
+                                        pengembangan sistem untuk rekrutmen, e-commerce, e-learning, dll. Kami dapat menjadi
+                                        mitra
+                                        terbaik untuk membangun sistem manajemen Anda.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse8">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Kami ingin membuat kontrak kemitraan / distributor dengan LOGIQUE.
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse8" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Mari bicarakan hal ini secara lebih rinci. Silakan kunjungi kantor kami jika Anda
+                                        berencana ke Jakarta atau mintalah kami untuk datang ke kantor Anda kapan saja.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse9">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Kami ingin membangun aplikasi mobile.
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse9" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Kami mampu mengembangkan aplikasi Android dan iOS, Hybrid App, dan PWA. Beri tahulah
+                                        kami
+                                        tentang kebutuhan aplikasi mobile Anda.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse10">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Kami ingin mencetak alat pemasaran, apakah Anda dapat menawarkan pencetakan
+                                            sekaligus
+                                            dengan jasa desain?
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse10" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Kami dapat menyediakan layanan desain dan pencetakan. Kami juga dapat mendesain dan
+                                        mencetak produk lain seperti T-shirt atau spanduk.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <dt class="panel-title" type="button" data-toggle="collapse" data-parent="#accordion" data-target="#collapse11">
+                                        <i class="fa fa-question-circle">&nbsp;</i><a>
+                                            Apakah Anda mampu menerapkan SEO dalam Bahasa Inggris?
+                                        </a>
+                                    </dt>
+                                </div>
+                                <div id="collapse11" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        Ya, dan kami memiliki pengalaman yang terbukti unggul di bidang tersebut; membantu
+                                        banyak
+                                        perusahaan keuangan, travel,
+                                        dan pendidikan untuk mempromosikan situs web mereka sehingga bisa ditemukan di posisi
+                                        teratas di hasil pencarian. Tim kami,
+                                        yang terdiri dari banyak penulis dan pemasar konten Indonesia, akan membantu
+                                        memberikan
+                                        Anda hasil SEO terbaik. Kami juga dapat menerapkan SEO dalam Indonesia dan Jepang.
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
         </div>
 
+<<<<<<< HEAD:layanan/harga-pembuatan-website.php
+       
+        
+       
+
+            <script type="application/ld+json">
+=======
         <script type="application/ld+json">
+>>>>>>> develop:layanan/biaya-pembuatan-website.php
             {
                 "@context": "https://schema.org",
                 "@type": "FAQPage",
@@ -751,8 +997,11 @@ if (isset($_POST['submitted'])) {
                 }]
             }
         </script>
+<<<<<<< HEAD:layanan/harga-pembuatan-website.php
+=======
 
 
+>>>>>>> develop:layanan/biaya-pembuatan-website.php
         <?php include '../footer.php'; ?>
     </div>
 </body>
