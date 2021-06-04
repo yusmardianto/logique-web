c291cffd5bbd015178a2ca878b5f9fd831277039
abdul_rachman <abdul.rachman@logique.co.id>
2021-06-04 21:46:23 +0700

    fix : carousel mobile why logique section

--- layanan/devops-why-logique-mobile-section.php
+++ layanan/devops-why-logique-mobile-section.php
@@ -15,54 +15,44 @@
                     <img src="/img/logo.png" alt="Jasa Pembuatan Website Logique Digital Indonesia" class="img-responsive logo--update-design">
                 </a> 
                                      
-                <div class="carousel-inner" >                        
-                    <!-- <div class="item active" style="height: 115% !important;" >                                        
-                        <img class="mt-7 centering-image" style="width: 100%; border-radius: 20px;" src="assets/img/img-devops/bg-why-logique-1.webp" alt="bg-1">
-                        <div class="carousel-caption text-left" >        
-                            <h3 class="text-color-black-main text-left -ml-6-sm ">
-                                    Memberikan solusi lengkap dan terbaik
-                                </h3>                        
-                            <p class="text-color-black-main mt-2-sm -ml-6-sm ">
-                                LOGIQUE menyediakan layanan penuh DevOps pada cloud server. Kami juga mampu memberikan beragam solusi terbaik mulai dari pengelolaan cloud server, pengembangan aplikasi dan website, keamanan cyber, dan lain-lain. Bersama LOGIQUE, Anda akan mendapatkan One Stop Solution untuk mengembangkan bisnis dengan mudah dan sukses di era digital.  
-                            </p>
-                        </div>
-                    </div> -->
+                <div class="carousel-inner" >                                       
                     <div class="item active"> 
-                        <div style="display: flex; flex-direction: column; justify-content:center">
+                        <div class="flex flex-col justify-center">
                             <img class="mt-7 centering-image" style="width: 100%; border-radius: 20px;" src="assets/img/img-devops/bg-why-logique-1.webp" alt="bg-1">                                                        
                         </div>
-                        <div style="padding:16px">
+                        <div class="p-6">
                             <h3 class="text-color-black-main text-left">
                                 Memberikan solusi lengkap dan terbaik
                             </h3>    
                             <p class="text-color-black-main mt-2-sm  ">
                                 LOGIQUE menyediakan layanan penuh DevOps pada cloud server. Kami juga mampu memberikan beragam solusi terbaik mulai dari pengelolaan cloud server, pengembangan aplikasi dan website, keamanan cyber, dan lain-lain. Bersama LOGIQUE, Anda akan mendapatkan One Stop Solution untuk mengembangkan bisnis dengan mudah dan sukses di era digital.  
                             </p>
-                        </div>
-                        <!-- <div class="carousel-caption text-left" >  
-                        </div> -->
-                    
+                        </div>                                          
                     </div>
                                     
-                    <div class="item" style="height: 110% !important;; ">
-                        <img class="mt-7 centering-image" style="width: 100%; border-radius: 20px;" src="assets/img/img-devops/bg-why-logique-2.webp" alt="bg-2" >
-                        <div class="carousel-caption">
-                            <h3 class="text-color-black-main text-left -ml-6-sm ">
+                    <div class="item" >
+                        <div class="flex flex-col justify-center">
+                            <img class="mt-7 centering-image" style="width: 100%; border-radius: 20px;" src="assets/img/img-devops/bg-why-logique-2.webp" alt="bg-2" >
+                        </div>
+                        <div class="p-6">
+                            <h3 class="text-color-black-main text-left  ">
                                 Profesional dan berpengalaman
                             </h3>                       
-                            <p class="text-color-black-main text-left mt-2-sm -ml-6-sm ">
+                            <p class="text-color-black-main text-left mt-2-sm  ">
                                 LOGIQUE adalah mitra AWS yang sudah tersertifikasi di Indonesia. Selain itu, LOGIQUE juga memiliki tim ahli di bidang pengelolaan cloud server yang sudah sangat berpengalaman dalam membantu berbagai jenis bisnis di Indonesia. Oleh karena itu, Anda tidak perlu meragukan keahlian teknis yang kami miliki.
                             </p>
                         </div>
                     </div>
 
-                    <div class="item"  style="height: 100% !important;">
-                        <img class="mt-7 centering-image" style="width: 100%; border-radius: 20px;" src="assets/img/img-devops/bg-why-logique-3.webp" alt="bg-3" >                        
-                        <div class="carousel-caption">
-                            <h3 class="text-color-black-main text-left -ml-6-sm ">
+                    <div class="item">
+                        <div class="flex flex-col justify-center">
+                            <img class="mt-7 centering-image" style="width: 100%; border-radius: 20px;" src="assets/img/img-devops/bg-why-logique-3.webp" alt="bg-3" >                        
+                        </div>
+                        <div class="p-6">
+                            <h3 class="text-color-black-main text-left ">
                                 Mampu memberikan respon yang cepat
                             </h3>                       
-                            <p class="text-color-black-main text-left mt-2-sm -ml-6-sm ">
+                            <p class="text-color-black-main text-left mt-2-sm ">
                                 LOGIQUE memberikan layanan DevOps yang fast respon untuk kebutuhan cloud server Anda. Kami juga akan memberikan dukungan teknis dan trouble shooting 24/7 jika terjadi masalah yang tidak diinginkan pada cloud server Anda.
                             </p>
                         </div>
