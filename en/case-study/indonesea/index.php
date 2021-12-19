83e2a484f7e5a831f6565e28ea87977eaea698ef
abdul_rachman <abdul.rachman@logique.co.id>
2021-12-19 16:32:24 +0700

    fix(case-study-indonesea-en): use css & font only from root case study indonesea ina folder

--- en/case-study/indonesea/index.php
+++ en/case-study/indonesea/index.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="id">
   <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0">
-  <meta name="description" itemprop="description" content="PT. Jelajah Bahari Indonesia (Indonesea) has entrusted LOGIQUE with developing the Indonesea.id website by using an efficient Dive Booking System.">
-	<meta name="keywords" content="jasa pembuatan website, jasa pembuatan aplikasi, jasa seo, jasa promosi online, digital marketing, web developer, desain web, logique">
-	<meta name="author" content="PT. Logique Digital Indonesia">
-	<meta itemprop="name" content="PT. Logique Digital Indonesia">
-	<meta property="og:title" content="Website Creation & Development for Indonesea | LOGIQUE">
-	<meta property="og:type" content="website">
-	<meta property="og:site_name" content="Logique">
-	<meta property="og:url" content=" https://www.logique.co.id/en/case-study/indonesea/">
-	<meta property="og:image" content="https://www.logique.co.id/img/ogimg/case-study.png">
-	<meta property="og:description" content="PT. Jelajah Bahari Indonesia (Indonesea) has entrusted LOGIQUE with developing the Indonesea.id website by using an efficient Dive Booking System.">
-	<link rel="amphtml" href="https://www.logique.co.id/amp/index.html">
-  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-  <script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  </script>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&amp;display=swap" rel="stylesheet">
-	ga('create', 'UA-105878648-1', 'auto');
-	ga('send', 'pageview');
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0">
+    <meta name="description" itemprop="description" content="PT. Jelajah Bahari Indonesia (Indonesea) mempercayakan LOGIQUE dalam membuat & mengembangkan website Indonesea.id sebagai Dive Booking System yang mudah & efisien.">
+    <meta name="keywords" content="jasa pembuatan website, jasa pembuatan aplikasi, jasa seo, jasa promosi online, digital marketing, web developer, desain web, logique">
+    <meta name="author" content="PT. Logique Digital Indonesia">
+    <meta itemprop="name" content="PT. Logique Digital Indonesia">
+    <meta property="og:title" content="Pembuatan & Pengembangan Website Indonesea.id | LOGIQUE">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Logique">
+    <meta property="og:url" content="https://www.logique.co.id/case-study/jba-auction-system/">
+    <meta property="og:image" content="https://www.logique.co.id/img/ogimg/case-study.png">
+    <meta property="og:description" content="PT. Jelajah Bahari Indonesia (Indonesea) mempercayakan LOGIQUE dalam membuat & mengembangkan website Indonesea.id sebagai Dive Booking System yang mudah & efisien.">
+    <link rel="amphtml" href="https://www.logique.co.id/amp/index.html">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="canonical" href="https://www.logique.co.id/case-study/indonesea/"/>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-105878648-1', 'auto');
+    ga('send', 'pageview');
 
 	</script>
   <title>Web Creation & Development for Indonesea.id | LOGIQUE</title>
@@ -34,8 +34,11 @@
 
   <!-- <link href="../../../css/style.css" rel="stylesheet">  -->
    <link rel="stylesheet" href="../../../css/sidebar-update.css" >
-  <!-- case study style -->
-  <link href="css/new-style.css" rel="stylesheet">
+  <!-- case study style -->  
+  <link href="../../../case-study/indonesea/css/new-style.css" rel="stylesheet">    
+  <link href="../../../css/newstyle.css" rel="stylesheet">
+
+
 
     </head>
 	<body> 
@@ -149,8 +152,8 @@
               </div>
           </div>
         </div> 
-        <h1 class="cs-title">Indonesea<br><i>Increasing Sales of Units<br>
-Through Targeted Conversions</i></h1>
+        <h1 class="cs-title">Indonesea</h1>
+          <!-- <br><i>Increasing Sales of Units<br>Through Targeted Conversions</i> -->
         </section>
         <section class='cs-section client-info'>
             <div class="row">
