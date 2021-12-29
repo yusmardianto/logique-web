cf8b38c5db9b8f7f65243e03ea6d00c6d6bd7222
abdul_rachman <abdul.rachman@logique.co.id>
2021-12-29 07:38:04 +0700

    refactor(case-study-jpn): reduce some css & fonts folder

--- jp/case-study/pan-pacific/index.php
+++ jp/case-study/pan-pacific/index.php
@@ -30,8 +30,8 @@
   <!-- <link href="../../../css/style.css" rel="stylesheet">  -->
    <link rel="stylesheet" href="../../../css/sidebar-update.css" >
   <!-- case study style -->
-  <link href="css/new-style.css" rel="stylesheet">
-
+  <link href="../../../case-study/pan-pacific/css/new-style.css" rel="stylesheet">
+  <link href="../../../css/newstyle.css" rel="stylesheet">
     </head>
 	<body>
 	 
