7e4f185a7f7df9048e6db8e4d773085cceea6cce
Yusmar <yusmardianto@logique.co.id>
2022-05-30 15:17:49 +0700

    add download pdf button

--- downloadpdf.php
+++
@@ -1,5 +0,0 @@
-<?php
- header('Content-disposition: attachment; filename=Company_Profile_ID.pdf');
- header('Content-type: application/pdf');
- readfile('download/Company_Profile_ID.pdf');
- ?>
--- en/downloadpdf.php
+++
@@ -1,5 +0,0 @@
-<?php
- header('Content-disposition: attachment; filename=Company_Profile_-_EN-Version.pdf');
- header('Content-type: application/pdf');
- readfile('download/Company_Profile_-_EN-Version.pdf');
- ?>
