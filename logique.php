8b46f3135d5a35cf42b4f12c60265dfe76309251
yusmardianto <yyusmar2@gmail.com>
2022-06-22 13:46:28 +0700

    about us update

--- logique.php
+++ logique.php
@@ -142,69 +142,83 @@
 
             <section id="principle">
                 <div class="container__">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="custombox__slider">
-                                <div class="custombox__custom-slider">
-                                    <div id="profile" class="carousel slide" data-ride="carousel" data-interval="3000">
-                                        <div class="carousel-inner">
-                                            <!-- <div class="item ">
-                                                    <img src="/img/banner-logo-logique.jpg" alt="Web Developer LOGIQUE 5">
-                                                    <div class="custombox__slider-text-wrapper">
-                                                        <p class="custombox__slider-text">Dengan pengalaman lebih dari 7
-                                                            tahun
-                                                            di Indonesia, kami dapat memecahkan masalah yang pada umumnya
-                                                            terjadi di Indonesia.</p>
+                    <div class="custombox__custom-slider">
+                        <div id="profile" class="carousel slide" data-ride="carousel" data-interval="false">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="carousel-inner">
+                                        <div class="item active">
+                                            <img style="object-position:0 0" src="/img/slider1.jpg" alt="Web Developer LOGIQUE 1">
+                                        </div>
+                                        <div class="item">
+                                            <img src="/img/slider5.jpg" alt="Web Developer LOGIQUE 5">
+                                        </div>
+                                        <div class="item">
+                                            <img style="object-position:0 0" src="/img/banner-all-member.jpg" alt="logique member">
+                                        </div>
+                                        <div class="item">
+                                            <img src="/img/slider4.jpg" alt="Web Developer LOGIQUE 4">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="bg--dark">
+                                        <p align="center">Menyediakan layanan IT dan Digital Marketing</p>
+                                    </div><br>
+                                    <div>
+                                        <div class="custombox__custom-slider">
+                                            <div id="profile" class="carousel slide" data-ride="carousel" data-interval="false">
+                                                <div class="carousel-inner">
+                                                    <div class="item active">
+                                                        <p><b>Konsultasi dengan kami agar kami dapat membantu mengembangkan bisnis Anda.</b></p>
                                                     </div>
-                                            </div> -->
-                                            <div class="item active">
-                                                <img style="object-position:0 0" src="/img/slider1.jpg"
-                                                        alt="Web Developer LOGIQUE 1">
-                                                <div class="custombox__slider-text-wrapper">
-                                                    <p class="custombox__slider-text">Konsultasi dengan kami agar kami dapat membantu mengembangkan bisnis Anda.
-                                                    </p>
+                                                    <div class="item">
+                                                        <p><b>Dengan lebih dari 7 tahun pengalaman di Indonesia, tim kami siap untuk menyelesaikan berbagai permasalahan IT Anda.</b></p>
+                                                    </div>
+                                                    <div class="item">
+                                                        <p><b>Kami menyediakan software monitoring sebagai solusi untuk meningkatkan produktivitas.</b></p>
+                                                    </div>
+                                                    <div class="item">
+                                                        <p><b>Kami menghasilkan software berkualitas yang mengikuti standar jepang / internasional, untuk memenuhi permintaan klien secara global.</b></p>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="item">
-                                                <img src="/img/slider5.jpg" alt="Web Developer LOGIQUE 5">
-                                                <div class="custombox__slider-text-wrapper">
-                                                    <p class="custombox__slider-text">Dengan lebih dari 7 tahun pengalaman di Indonesia, tim kami siap untuk menyelesaikan berbagai permasalahan IT Anda.</p>
+                                        </div>
+                                        <div class="row" style="margin-top:105px">
+                                            <div class="col-md-1">
+                                                <div class="bg--button">
+                                                    <a class="left" href="#profile" data-slide="prev">
+                                                        <div class="col-md-2">
+                                                            <img src="/img/logique/download.png"/>
+                                                        </div>
+                                                    </a>
                                                 </div>
                                             </div>
-                                            <div class="item">
-                                                <img style="object-position:0 0" src="/img/banner-all-member.jpg"
-                                                        alt="logique member">
-                                                <div class="custombox__slider-text-wrapper">
-                                                    <p class="custombox__slider-text">Kami menyediakan software monitoring sebagai solusi untuk meningkatkan produktivitas.</p>
-                                                </div>
-                                            </div>
-                                            <div class="item">
-                                                <img src="/img/slider4.jpg" alt="Web Developer LOGIQUE 4">
-                                                <div class="custombox__slider-text-wrapper">
-                                                    <p class="custombox__slider-text">Kami menghasilkan software berkualitas yang mengikuti standar jepang / internasional, untuk memenuhi permintaan klien secara global. 
-                                                    </p>
+                                            <div class="col-md-1">
+                                                <div class="col-md-2">
+                                                    <div class="bg--button">
+                                                        <a class="right" href="#profile" data-slide="next">
+                                                            <div class="col-md-2">
+                                                                <span><img src="/img/logique/download.png"/></span>
+                                                            </div>
+                                                        </a>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                         <!-- Left and right controls -->
-                                        <a class="left carousel-control" href="#profile" data-slide="prev">
-                                            <span class="glyphicon glyphicon-chevron-left"></span>
-                                            <span class="sr-only">Previous</span>
-                                        </a>
-                                        <a class="right carousel-control" href="#profile" data-slide="next">
-                                            <span class="glyphicon glyphicon-chevron-right"></span>
-                                            <span class="sr-only">Next</span>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-6">
-                            <div class="principle">
-                                <div class="principle__content">
-                                    <div class="bg--dark">
-                                        <p>Menyediakan layanan IT dan Digital Marketing</p>
+                                        <!-- <div style="margin-top:160px">
+                                            <a class="left" href="#profile" data-slide="prev">
+                                                <div class="col-md-2">
+                                                    <img src="/img/logique/arrow.png" />
+                                                </div>
+                                            </a>
+                                            <a class="right" href="#profile" data-slide="next">
+                                                <div class="col-md-2">
+                                                    <img src="/img/logique/arrow.png" />
+                                                </div>
+                                            </a>
+                                        </div> -->
                                     </div>
                                 </div>
                             </div>
@@ -213,7 +227,7 @@
                 </div>
             </section>
 
-            <section id="principle">
+            <section id="principle" style="margin-top:-500px">
                 <div class="container__">
                     <h1 class="title-index"><b>Karakteristik LOGIQUE</b></h1>
                     <hr class="title__yellow-line">
@@ -375,47 +389,49 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="bg--orange col-md-6">
+                        <div class="bg--orange col-md-5">
                             <div class="row">
                                 <div class="col-md-2">
                                     <img src="/img/logique/Group311.png"/>
                                 </div>
                                 <div class="col-md-10">
-                                    <p>Web App Development Service</p>
-                                    <p>Optimize Your Website on Every Search Engine</p>
+                                    <p style="font-size:15px"><b>Web App Development Service</b></p>
+                                    <p style="font-size:15px"><b>Optimize Your Website on Every Search Engine</b></p>
                                 </div>
                             </div>
                         </div>
-                        <div class="bg--orange col-md-6">
+                        <div class="col-md-1"></div>
+                        <div class="bg--orange col-md-5">
                             <div class="row">
                                 <div class="col-md-2">
                                     <img src="/img/logique/Group333.png"/>
                                 </div>
                                 <div class="col-md-10">
-                                    <p>SEO Service</p>
-                                    <p>Optimize Your Website on Every Search Engine</p>
+                                    <p style="font-size:15px"><b>SEO Service</b></p>
+                                    <p style="font-size:15px"><b>Optimize Your Website on Every Search Engine</b></p>
                                 </div>
                             </div>
                         </div><br><br><br><br>
-                        <div class="bg--orange col-md-6">
+                        <div class="bg--orange col-md-5">
                             <div class="row">
                                 <div class="col-md-2">
                                     <img src="/img/logique/Group301@2x.png"/>
                                 </div>
                                 <div class="col-md-10">
-                                    <p>IT Security Vulnerability Assessment Service</p>
-                                    <p>Optimize Your Website on Every Search Engine</p>
+                                    <p style="font-size:15px"><b>IT Security Vulnerability Assessment Service</b></p>
+                                    <p style="font-size:15px"><b>Optimize Your Website on Every Search Engine</b></p>
                                 </div>
                             </div>
                         </div>
-                        <div class="bg--orange col-md-6">
+                        <div class="col-md-1"></div>
+                        <div class="bg--orange col-md-5">
                             <div class="row">
                                 <div class="col-md-2">
                                     <img src="/img/logique/Group125@2x.png"/>
                                 </div>
                                 <div class="col-md-10">
-                                    <p>IT Professional Staffing Service (by KAZOKKU)</p>
-                                    <p>Outsource IT professional Resource</p>
+                                    <p style="font-size:15px"><b>IT Professional Staffing Service (by KAZOKKU)</b></p>
+                                    <p style="font-size:15px"><b>Outsource IT professional Resource</b></p>
                                 </div>
                             </div>
                         </div>
